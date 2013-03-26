0ce1fc1b0d98bb7ec19a9bcebc8c0daef9388048
Sam Joseph <tansaku@gmail.com>
2013-03-26 09:23:35 -0700

    slight variation

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -6,7 +6,7 @@ Then /^I should see "(.*?)", "(.*?)" and "(.*?)" in the map$/ do |name1, name2, 
   org1 = Organization.find_by_name(name1)
   org2 = Organization.find_by_name(name2)
   org3 = Organization.find_by_name(name3)
-  page.should have_xpath "//script[contains(.,'Gmaps.map.markers = [{\"lat\":#{org3.latitude},\"lng\":#{org3.longitude}},{\"lat\":#{org2.latitude},\"lng\":#{org2.longitude}},{\"lat\":#{org1.latitude},\"lng\":#{org1.longitude}}];')]"
+  page.should have_xpath "//script[contains(.,'Gmaps.map.markers = [{\"lat\":#{org3.latitude},\"lng\":#{org3.longitude}},{\"lat\":#{org2.latitude},\"lng\":#{org2.longitude}},{\"lat\":#{org1.latitude},\"lng\":#{org1.longitude}')]"
 
 #Gmaps.map.markers = [{"lat":51.5814093,"lng":-0.343908},{"lat":51.5813411,"lng":-0.3468075},{"lat":51.58125769999999,"lng":-0.3473078}];
 #Gmaps.map.markers = [{"lat":51.5814093,"lng":-0.343908},{"lat":51.58125769999999,"lng":-0.3473078},{"lat":51.5813411,"lng":-0.3468075}];)]
