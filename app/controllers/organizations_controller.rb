5613684b323e7733f906406ff69275c85d9e7055
Pavlo Osadchyi <posadchiy@gmail.com>
2013-04-06 21:06:00 +0300

    Calls to gmaps API was stubbed in specs

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -54,6 +54,7 @@ class OrganizationsController < ApplicationController
   # POST /organizations.json
   def create
     @organization = Organization.new(params[:organization])
+
     respond_to do |format|
       if @organization.save
         format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
