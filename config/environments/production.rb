5954f59e074db7756b03a3795a3a13c18307f7fb
Sam Joseph <tansaku@gmail.com>
2013-05-21 11:48:31 +0100

    mail configuration for heroku - TODO also adapt this for multiple staging and production on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ LocalSupport::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'harrowcn-staging-eu.herokuapp.com/' }
 end
