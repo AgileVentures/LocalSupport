38fc29d31ed223008a854acd2f474df3200fbe49
juanoliver <juan.oliver@olvasoft.com>
2013-05-24 21:02:21 +0000

    organization in ascending - descending order. Model method needs to be modified

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,14 @@
 class OrganizationsController < ApplicationController
-  # GET /organizations/search
+
+  OrganizationsController::ALPHABETICALLY='ALPHABETICALLY'
+  OrganizationsController::UPDATE_DATE='UPDATE_DATE'
+  OrganizationsController::DESCENDING='DESCENDING'
+  OrganizationsController::ASCENDING='ASCENDING'
+
+  @order = nil
+  @method = nil
+
+# GET /organizations/search
   # GET /organizations/search.json
   before_filter :authenticate_charity_worker!, :except => [:search, :index, :show]
   def search
@@ -16,6 +25,27 @@ class OrganizationsController < ApplicationController
   # GET /organizations
   # GET /organizations.json
   def index
+    @order = nil
+    @method = nil
+    if params[:method]&& params[:order] then
+      @method=params[:method]
+      session[:method]=params[:method]
+      @order=params[:order]
+      session[:order]=params[:order]
+    elsif params[:method]
+      @method=params[:method]
+      session[:method]=params[:method]
+      params[:order]=load_params_order
+    elsif params[:order]
+      @order=params[:order]
+      session[:order]=params[:order]
+      params[:method]=load_params_method
+    else
+      params[:order]=load_params_order
+      session[:order]=params[:order]
+      params[:method]=load_params_method
+      session[:method]=params[:method]
+    end
     @organizations = Organization.order("updated_at DESC")
     @json = @organizations.to_gmaps4rails
     respond_to do |format|
@@ -23,6 +53,28 @@ class OrganizationsController < ApplicationController
       format.json { render json: @organizations }
     end
   end
+
+
+  def load_params_order
+    if session[:order]
+       @order=session[:order]
+       session[:order]
+    else
+      @order=OrganizationsController::UPDATE_DATE
+      OrganizationsController::UPDATE_DATE
+    end
+  end
+
+  def load_params_method
+    if session[:method]
+       @method=session[:method]
+       session[:method]
+    else
+      @method=OrganizationsController::DESCENDING
+      OrganizationsController::DESCENDING
+    end
+  end
+
 
   # GET /organizations/1
   # GET /organizations/1.json
@@ -34,6 +86,7 @@ class OrganizationsController < ApplicationController
       format.json { render json: @organization }
     end
   end
+
 
   # GET /organizations/new
   # GET /organizations/new.json
