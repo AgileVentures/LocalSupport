dea2077dcc0642298eb35a4de3d87c2218191e21
mtc2013 <mtc2013opensource@gmail.com>
2013-05-31 14:53:10 -0400

    Another test spec case for find_by_admin false

--- spec/models/charity_worker_spec.rb
+++ spec/models/charity_worker_spec.rb
@@ -5,14 +5,20 @@ describe CharityWorker do
     FactoryGirl.factories.clear
     FactoryGirl.find_definitions
   end
-  it 'must find an admin in find_by_admin with true argument' do
+  before :each do
     FactoryGirl.build(:charity_worker, :email => 'jj@example.com', 
       :password => 'pppppppp', :admin => true).save!
     FactoryGirl.build(:charity_worker, :email => 'jj1@example.com', 
       :password => 'pppppppp', :admin => true).save!
     FactoryGirl.build(:charity_worker, :email => 'jj2@example.com', 
       :password => 'pppppppp', :admin => false).save!
+  end
+  it 'must find an admin in find_by_admin with true argument' do
     @result = CharityWorker.find_by_admin(true)
     expect(@result.admin?).to be_true
+  end
+  it 'must find a non-admin in find_by_admin with false argument' do
+    @result = CharityWorker.find_by_admin(false)
+    expect(@result.admin?).to be_false
   end
 end
