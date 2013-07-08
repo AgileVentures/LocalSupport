8b6274bff7a278e40b4841ba0350359f850a80ef
Sam Joseph <tansaku@gmail.com>
2013-07-08 18:29:53 +0100

    annoying bit of regex was not quite catching what we wanted for matching the list of marker popup stuff in the gmaps javascript - fixed now, still brittle - would like a better wway to test

--- features/step_definitions/map_steps.rb
+++ features/step_definitions/map_steps.rb
@@ -72,10 +72,10 @@ end
 Then /^the coordinates for "(.*?)" and "(.*?)" should( not)? be the same/ do | org1_name, org2_name, negation|
   #Gmaps.map.markers = [{"description":"<a href=\"/organizations/1320\">test</a>","lat":50.3739788,"lng":-95.84172219999999}];
 
-  matches = page.html.match %Q<{\\"description\\":\\".*>#{org1_name}</a>.*\\",\\"lat\\":((?:-|)\\d+\.\\d+),\\"lng\\":((?:-|)\\d+\.\\d+)}>
+  matches = page.html.match %Q<{\\"description\\":\\"[^}]*#{org1_name}[^}]*\\",\\"lat\\":((?:-|)\\d+\.\\d+),\\"lng\\":((?:-|)\\d+\.\\d+)}>
   org1_lat = matches[1]
   org1_lng = matches[2]
-  matches = page.html.match %Q<{\\"description\\":\\".*>#{org2_name}</a>.*\\",\\"lat\\":((?:-|)\\d+\.\\d+),\\"lng\\":((?:-|)\\d+\.\\d+)}>
+  matches = page.html.match %Q<{\\"description\\":\\"[^}]*#{org2_name}[^}]*\\",\\"lat\\":((?:-|)\\d+\.\\d+),\\"lng\\":((?:-|)\\d+\.\\d+)}>
   org2_lat = matches[1]
   org2_lng = matches[2]
   lat_same = org1_lat == org2_lat
