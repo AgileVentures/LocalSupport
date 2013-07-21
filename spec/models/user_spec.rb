842a5d2f3434054151fd69df9d9d881eb751602b
Josh Chernoff <joshc@digitalcak.es>
2013-07-21 18:10:03 -0500

    Refactored User Spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,20 +29,21 @@ describe User do
     
     let( :non_associated_model ) { mock_model("Organization") }
     
-    subject { create(:user, admin: false ) } 
-
+    subject { create(:user, admin: false, organization: model ) } 
+    
     it 'can edit associated organization' do
-      should_receive(:organization).and_return model
+      subject.organization.should eq model
       subject.can_edit?(model).should be_true 
     end
     
     it 'can not edit non-associated organization' do
-      should_receive(:organization).and_return model
+      subject.organization.should eq model
       subject.can_edit?(non_associated_model).should be_false
     end
     
     it 'can not edit when associated with no org' do
-      should_receive(:organization).and_return nil
+      subject.organization = nil
+      subject.organization.should eq nil
       subject.can_edit?(non_associated_model).should be_false
     end
 
@@ -53,7 +54,7 @@ describe User do
   end
 
   it 'does not allow mass assignment of admin for security' do
-    user = create(:user, admin: false)
+    user = FactoryGirl.create(:user, admin: false)
     user.update_attributes(:admin=> true)
     user.save!
     user.admin.should be_false
