86772c25d45db067b1f6c4e3bb838fa5fe3af29e
Josh Chernoff <joshc@digitalcak.es>
2013-07-21 22:26:52 -0500

    Updated subject to be user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,11 +16,18 @@ describe User do
     result.admin?.should be_false
   end
 
+  it 'does not allow mass assignment of admin for security' do
+    user = FactoryGirl.create(:user, admin: false)
+    user.update_attributes(:admin=> true)
+    user.save!
+    user.admin.should be_false
+  end
+
   context 'is admin' do
-    subject { create(:user, admin: true) }  
+    subject(:user) { create(:user, admin: true) }  
     
     it 'can edit organizations' do
-      subject.can_edit?(model).should be_true 
+      user.can_edit?(model).should be_true 
     end
 
   end
@@ -29,35 +36,28 @@ describe User do
     
     let( :non_associated_model ) { mock_model("Organization") }
     
-    subject { create(:user, admin: false, organization: model ) } 
+    subject(:user) { create(:user, admin: false, organization: model ) } 
     
     it 'can edit associated organization' do
-      subject.organization.should eq model
-      subject.can_edit?(model).should be_true 
+      user.organization.should eq model
+      user.can_edit?(model).should be_true 
     end
     
     it 'can not edit non-associated organization' do
-      subject.organization.should eq model
-      subject.can_edit?(non_associated_model).should be_false
+      user.organization.should eq model
+      user.can_edit?(non_associated_model).should be_false
     end
     
     it 'can not edit when associated with no org' do
-      subject.organization = nil
-      subject.organization.should eq nil
-      subject.can_edit?(non_associated_model).should be_false
+      user.organization = nil
+      user.organization.should eq nil
+      user.can_edit?(non_associated_model).should be_false
     end
 
     it 'can not edit when associated with no org and attempting to access non-existent org' do
-      subject.can_edit?(nil).should be_false
+      user.can_edit?(nil).should be_false
     end
    
-  end
-
-  it 'does not allow mass assignment of admin for security' do
-    user = FactoryGirl.create(:user, admin: false)
-    user.update_attributes(:admin=> true)
-    user.save!
-    user.admin.should be_false
   end
 
 end
