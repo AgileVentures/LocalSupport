77919914494a8157c62269e65917056006d709ae
Rafael George <george.rafael@gmail.com>
2013-09-02 18:21:38 -0400

    Move call to CreateOrganizationFromArray#check_columns_in to constructor
    - Rename CreateOrganizationFromArray#check_columns_in to CreateOrganizationFromArray#normalize, better name for what's the code is doing

--- app/models/create_organization_from_array.rb
+++ app/models/create_organization_from_array.rb
@@ -2,11 +2,10 @@ class CreateOrganizationFromArray
 
   def initialize(row, mappings)
     @row = row
-    @mappings = mappings
+    @mappings = normalize(mappings)
   end
 
   def call(validate)
-    check_columns_in
     return nil if @row[@mappings[:date_removed]]
     return nil if Organization.find_by_name(organization_name)
 
@@ -21,8 +20,8 @@ class CreateOrganizationFromArray
     @organization_name ||= FirstCapitalsHumanizer.call(@row[@mappings[:name]])
   end
 
-  def check_columns_in
-    @mappings.each_value do |column_name|
+  def normalize(mappings)
+    mappings.each_value do |column_name|
       unless @row.header?(column_name)
         raise CSV::MalformedCSVError, "No expected column with name #{column_name} in CSV file"
       end
