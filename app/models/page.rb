a6a3c729feeb5244af7f3c8a8d7a84ed291d9b2d
johnnymo87 <johnnymo87@gmail.com>
2013-09-25 17:28:13 -0700

    added validations to page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveRecord::Base
   attr_accessible :content, :name, :permalink
+  validates_presence_of :name, :permalink
   validates_uniqueness_of :permalink
 
   # Override method to allow static pages to be found via permalink instead of id
