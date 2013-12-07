eb9a563009b2e30b41b33d92cee5f1a9c4d63d07
Daniel Dawson <dpdawson@dpdawson.org>
2013-12-07 05:50:02 +0000

    Fix admin check in Users#index and remove unneeded comments in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def update
     user = User.find_by_id(params[:id])
     if params[:organization_id]
@@ -6,7 +7,6 @@ class UsersController < ApplicationController
       user.save!
       org = Organization.find(params[:organization_id])
       flash[:notice] = "You have requested admin status for #{org.name}"
-      #flash[:notice] = "You have requested admin status for My Organization"
       redirect_to(organization_path(params[:organization_id]))
     else
       redirect_to :status => 404 and return unless current_user.admin?
@@ -17,12 +17,11 @@ class UsersController < ApplicationController
   end
 
   def index
-    if !current_user.admin?
-      #flash[:notice] = "You must be signed in as an admin to perform this action!"
-      redirect_to root_path
-      flash[:notice] = "You must be signed in as an admin to perform this action!"
-    else
+    if current_user && current_user.admin?
       @users = User.all
+    else
+      flash[:notice] = "You must be signed in as an admin to perform this action!"
+      redirect_to root_path
     end
   end
 end
