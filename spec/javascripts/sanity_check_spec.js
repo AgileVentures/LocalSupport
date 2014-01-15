2d34cb6d5fa3ffac0bfb174b7f94fac064750f83
johnnymo87 <johnnymo87@gmail.com>
2014-01-15 20:52:48 +0000

    got jquery handler to load for jasmine, got spyOnEvent partially working

--- spec/javascripts/sanity_check_spec.js
+++ spec/javascripts/sanity_check_spec.js
@@ -3,15 +3,22 @@ describe('Jasmine sanity check', function() {
 });
 
 describe('what am i doing', function() {
+    var timo;
     beforeEach(function() {
         loadFixtures('TIMO_signed_out.html');
+        timo = $('#TIMO');
+        timo.TIMO();
     });
+
     it('My fixture should load', function() {
-        expect($('#TIMO')).toBe($('#TIMO'));
+        expect(timo).toBe(timo);
     });
     it('I should spy on fn TIMO', function() {
-        var spyEvent = spyOnEvent($('#TIMO'), 'click');
-//        $('#TIMO').click();
-        expect('click').toHaveBeenTriggeredOn($('#TIMO'))
+        // https://github.com/velesin/jasmine-jquery#event-spies
+        var spyEvent = spyOnEvent(timo, 'click');
+        $(timo).click();
+        expect('click').toHaveBeenTriggeredOn(timo);
+        // WHY DOESN'T THIS WORK?? It's pure copy-pasta.
+//        expect(spyEvent).toHaveBeenTriggered()
     });
 });
