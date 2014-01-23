2c5bc4f9addb77616dd21856eed8e007963b55ab
Sam Joseph <tansaku@gmail.com>
2014-01-23 11:19:37 +0000

    removing dead command and spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   layout 'full_width'
 
-
-
   def update
     user = User.find_by_id(params[:id])
     if params[:organization_id]
@@ -10,7 +8,6 @@ class UsersController < ApplicationController
       user.save!
       org = Organization.find(params[:organization_id])
       flash[:notice] = "You have requested admin status for #{org.name}"
-      #flash[:notice] = "You have requested admin status for My Organization"
       redirect_to(organization_path(params[:organization_id]))
     else
       redirect_to :status => 404 and return unless current_user.admin?
