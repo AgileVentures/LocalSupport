a76d4509d0dc6587fb704e71035884a37ff85a69
johnnymo87 <johnnymo87@gmail.com>
2014-01-29 22:23:03 +0000

    admins only

--- spec/controllers/orphans_controller_spec.rb
+++ spec/controllers/orphans_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe OrphansController do
-  #
+  it 'is for admins only' do
+    user = double('User', :admin? => false)
+    controller.stub(:current_user).and_return(user)
+    get :index
+    response.should redirect_to '/'
+    post :create, {}
+    response.should redirect_to '/'
+  end
 end
