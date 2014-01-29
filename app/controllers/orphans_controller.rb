a570473c98a500eac156abe8882b45e9024f0584
johnnymo87 <johnnymo87@gmail.com>
2014-01-29 18:52:17 +0000

    switched to sending strings

--- app/controllers/orphans_controller.rb
+++ app/controllers/orphans_controller.rb
@@ -3,28 +3,35 @@ class OrphansController < ApplicationController
   before_filter :authorize
 
   def index
-    @families = Organization.not_null_email.null_users + Organization.not_null_email.generated_users
-    @families = @families.map do |org|
-      if params[:id].to_i == org.id
-        user = Organization.find_by_id(params[:id]).generate_potential_user
-      else
-        user = org.users.present? ? org.users.first : NullObject.new
-      end
-      [org, user]
+    @families = Organization.not_null_email.null_users.map {|org| session[org.id].present? ? [org, session[org.id]] : [org, ''] }
+    @families += Organization.not_null_email.generated_users.map {|org| [org, org.users.first.reset_password_token]}
+  end
+
+  def create
+    user = Organization.find_by_id(params[:id]).generate_potential_user
+    if user.errors.any?
+      user = user.errors.full_messages.first
+    else
+      user = user.reset_password_token
+    end
+    respond_to do |format|
+      format.html { session[params[:id]] = user; redirect_to :index }
+      format.json { render :json => user.to_json }
     end
   end
 
-  def orphans_remote
-    @user = Organization.find_by_id(params[:id]).generate_potential_user
-    if @user.errors.any?
-      @user = @user.errors.full_messages.first
-    else
-      @user = @user.reset_password_token
-    end
-    respond_to do |format|
-      format.json { render :json => @user.to_json }
-    end
-  end
+
+  #def orphans_remote
+  #  @user = Organization.find_by_id(params[:id]).generate_potential_user
+  #  if @user.errors.any?
+  #    @user = @user.errors.full_messages.first
+  #  else
+  #    @user = @user.reset_password_token
+  #  end
+  #  respond_to do |format|
+  #    format.json { render :json => @user.to_json }
+  #  end
+  #end
 
   #def parents
   # result = Organization.find_by_id(params[:id]).generate_potential_user
