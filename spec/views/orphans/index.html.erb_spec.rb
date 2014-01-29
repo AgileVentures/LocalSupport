7a66f142cb3310fc661505c0a4f857142efa8a62
johnnymo87 <johnnymo87@gmail.com>
2014-01-29 17:56:30 +0000

    first test on view spec passing

--- spec/views/orphans/index.html.erb_spec.rb
+++ spec/views/orphans/index.html.erb_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe "orphans/index.html.erb" do
+  let(:org1) { stub_model Organization, name: 'test', address: '12 pinner rd', email: 'hello@there.com', users: [] }
+  let(:null_user) { stub_model NullObject }
+  before(:each) { assign(:families, [[org1, null_user]]) }
 
   it "should display a generate user button" do
     render
-    rendered.should_have content "Generate User"
+    rendered.should have_content "Generate User"
   end
 
 end
