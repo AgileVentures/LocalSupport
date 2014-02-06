6413f022106d8b23dfc0eefe4acaa184aede5678
johnnymo87 <johnnymo87@gmail.com>
2014-02-06 13:54:01 +0000

    oh, the syntax changed. derp.

--- spec/javascripts/generate_user_spec.js
+++ spec/javascripts/generate_user_spec.js
@@ -1,4 +1,3 @@
-//=require spec_helper
 describe('Generate User operation', function() {
     var generate_user, ajax;
     beforeEach(function() {
@@ -10,22 +9,22 @@ describe('Generate User operation', function() {
     it('makes an ajax request when clicked',function(){
         spyOn($, "ajax");
         generate_user.click();
-        var args = $.ajax.mostRecentCall.args[0];
+        var args = $.ajax.calls.mostRecent();
         expect(args.data).toEqual({ id: '362' });
         expect(args.dataType).toEqual('json');
         expect(args.type).toEqual('POST');
         expect(args.url).toEqual('/orphans')
     });
     it('inserts text and removes button if successful', function() {
-        spyOn( $, "ajax" ).andCallFake(function (params) { 
+        spyOn( $, "ajax" ).and.callFake(function (params) { 
           params.success("hi");
         });
         generate_user.click();
-        expect($('#362 span')).toHaveText('hi')
+        expect($('#362 span')).toHaveText('hi');
         expect($('#362 .response a').length).toBe(0)
     });
     it('inserts failure message if unsuccessful', function() {
-        spyOn( $, "ajax" ).andCallFake(function (params) { 
+        spyOn( $, "ajax" ).and.callFake(function (params) { 
           params.error("error");
         });
         generate_user.click();
