60f1173feea48258bfb561d669e2506d7179fff1
johnnymo87 <johnnymo87@gmail.com>
2014-02-12 17:26:24 +0000

    redirects to root if request.referer is somehow nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
     #  format.json { render :nothing => true, :status => 200 }
     #end
 
-    redirect_to request.referer
+    redirect_to request.referer || '/'
   end
 
   private
