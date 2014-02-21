aa4d99ad36c154d19c60193ff4196c7ce9fa486f
Rafael George <george.rafael@gmail.com>
2014-02-21 11:44:01 -0400

    Add new UserOrganizationClaimer service
    The service extract the business logic to request status and promote users to be admin to given organizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,50 @@ class UsersController < ApplicationController
   # business logic pulled into a separate model or process
   def update
     user = User.find_by_id(params[:id])
-    if params[:organization_id]
-      user.pending_organization_id = params[:organization_id]
-      user.save!
-      org = Organization.find(params[:organization_id])
-      flash[:notice] = "You have requested admin status for #{org.name}"
-      redirect_to(organization_path(params[:organization_id]))
-    else
-      redirect_to :status => 404 and return unless current_user.admin?
-      user.promote_to_org_admin
-      flash[:notice] = "You have approved #{user.email}."
-      redirect_to(users_path)
-    end
+    UserOrganizationClaimer.new(self, user, current_user).call(params[:organization_id])
   end
 
   def index
     @users = User.all
   end
+
+  def update_message_for_admin_status
+    org = Organization.find(params[:organization_id])
+    flash[:notice] = "You have requested admin status for #{org.name}"
+    redirect_to(organization_path(params[:organization_id]))
+  end
+
+  def update_message_promoting(user)
+    flash[:notice] = "You have approved #{user.email}."
+    redirect_to(users_path)
+  end
+
+  def update_failure
+    redirect_to :status => 404 
+  end
+end
+
+class UserOrganizationClaimer 
+
+  def initialize(listener, user, current_user)
+    @listener = listener
+    @user = user
+    @current_user = current_user
+  end
+
+  def call(organization_id)
+    if organization_id
+      user.pending_organization_id = organization_id
+      user.save!
+      listener.update_message_for_admin_status
+    elsif current_user.admin?
+      user.promote_to_org_admin
+      listener.update_message_promoting(user)
+    else 
+      listener.update_failure
+    end
+  end
+
+  private
+  attr_reader :listener, :user, :current_user
 end
---
+++ spec/services/users_organization_claimer_spec.rb
@@ -0,0 +1,3 @@
+describe 'UserOrganizationClaimer' do 
+
+end
