5880219f6a200052c4fff0e11011e8ab0b39e77c
Rafael George <george.rafael@gmail.com>
2014-02-25 15:53:02 -0400

    Remove some of the OrganizationReportsController specs
    They were redundant after adding the UserInviter service

--- spec/controllers/organization_reports_controller_spec.rb
+++ spec/controllers/organization_reports_controller_spec.rb
@@ -23,35 +23,4 @@ describe OrganizationReportsController do
     end
   end
 
-  describe '#without_users_create' do
-    let(:error) { double('ActiveModel::Errors') }
-    let(:params) { { values: [{id: 1, email: 'a@org.org'}, {id: 3, email: 'c@org.org'}] } }
-    before(:each) do
-      request.accept = 'application/json'
-      User.stub :find_by_email => nil
-      User.stub :invite! => user
-    end
-
-    it 'adds an error if the email is already in use by another user' do
-      post :without_users_create, params
-      res = ActiveSupport::JSON.decode(response.body)
-      res.should eq({'1' => 'Error: I-dentify target!', '3' => 'Error: I-dentify target!'})
-    end
-
-    it 'parses errors if there are any' do
-      user.stub :errors => error
-      error.stub :any? => true
-      error.stub :full_messages
-      error.stub_chain(:full_messages, :first).and_return('Ready to roll out!')
-      post :without_users_create, params
-      res = ActiveSupport::JSON.decode(response.body)
-      res.should eq({'1' => 'Error: Ready to roll out!', '3' => 'Error: Ready to roll out!'})
-    end
-
-    it 'elicits the reset password token otherwise' do
-      post :without_users_create, params
-      res = ActiveSupport::JSON.decode(response.body)
-      res.should eq({'1' => 'Invited!', '3' => 'Invited!'})
-    end
-  end
 end
