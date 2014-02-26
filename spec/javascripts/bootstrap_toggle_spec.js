c58dce31cbb869af672f9b542c124a568d96c7fb
mtc2013 <mtc2013opensource@gmail.com>
2014-02-26 08:15:56 -0500

    Add second jasmine spec for bootstrap toggle spec

--- spec/javascripts/bootstrap_toggle_spec.js
+++ spec/javascripts/bootstrap_toggle_spec.js
@@ -2,7 +2,7 @@ describe('bootstrap toggle', function() {
     var header;
     beforeEach(function() {
         setFixtures('<li class="dropdown" id="menuOrgs">'+
-            '<a class="dropdown-toggle" href="#">Organisations</a>' +
+            '<a class="dropdown-toggle" href="#" data-toggle ="dropdown">Organisations</a>' +
             '<ul class="dropdown-menu">' +
             '    <li><a href="/organization_reports/without_users">Without Users</a></li>' +
             '</ul>' +
@@ -12,7 +12,9 @@ describe('bootstrap toggle', function() {
     describe('when clicking on the parent', function() {
         it('pops up a list', function() {
             link.click();
-            expect($('li#menuOrgs').first()).toHaveClass('open');
+            expect($('li#menuOrgs')).toHaveClass('open');
+            link.click();
+            expect($('li#menuOrgs')).not.toHaveClass('open');
         });
     });
 });
