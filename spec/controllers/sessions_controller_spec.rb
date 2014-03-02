cb937abbc1e9e3d573f6e593c0e090f1063b6597
johnnymo87 <johnnymo87@gmail.com>
2014-03-02 05:50:06 +0000

    redirects determined by a whitelist of request controllers and verbs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -26,53 +26,5 @@ describe Devise::SessionsController do
       post :create, 'user' => {'email' => 'example@example.com', 'password' => '12345'}
       expect(flash[:alert]).to have_content "I'm sorry, you are not authorized to login to the system."
     end
-
-    describe 'non-admins' do
-      describe 'associated with an organization LOGS IN' do
-        it 'redirects to organization page' do
-          Gmaps4rails.stub(:geocode)
-          org = FactoryGirl.create(:organization, {:email => 'example@example.com'})
-          FactoryGirl.build(:user, {:email => 'example@example.com', :password => 'pppppppp', :organization => org}).save!
-          post :create, 'user' => {'email' => 'example@example.com', 'password' => 'pppppppp'}
-          expect(response).to redirect_to organization_path(org.id)
-        end
-      end
-      describe 'associated with nothing LOGS IN' do
-        it 'redirects to previous page after non-admin associated with nothing logs-in from charity page' do
-          request.stub(:path).and_return(organization_path(1))
-          #session.stub(:previous_url).and_return(organization_path(1))
-          FactoryGirl.build(:user, {:email => 'example@example.com', :password => 'pppppppp'}).save!
-          post :create, 'user' => {'email' => 'example@example.com', 'password' => 'pppppppp'}
-          expect(response).to redirect_to organization_path(1)
-        end
-
-        it 'redirects to root after non-admin associated with nothing logs-in from sign in page' do
-          FactoryGirl.build(:user, {:email => 'example@example.com', :password => 'pppppppp'}).save!
-          post :create, 'user' => {'email' => 'example@example.com', 'password' => 'pppppppp'}
-          expect(response).to redirect_to root_url
-        end
-
-        it 'redirects to home page after non-admin associated with nothing logs-in' do
-          FactoryGirl.build(:user, {:email => 'example@example.com', :password => 'pppppppp'}).save!
-          post :create, 'user' => {'email' => 'example@example.com', 'password' => 'pppppppp'}
-          expect(response).to redirect_to root_url
-        end
-      end
-      describe 'associated with nothing FAILS TO LOG IN' do
-        it 'renders sign in page after non-admin associated with nothing fails to log-in' do
-          FactoryGirl.build(:user, {:email => 'example@example.com', :password => 'pppppppp'}).save!
-          post :create, 'user' => {'email' => 'example@example.com', 'password' => '12345'}
-          expect(response).to be_ok
-        end
-
-        it 'displays warning flash after non-admin associated with nothing fails to log-in' do
-          FactoryGirl.build(:user, {:email => 'example@example.com', :password => 'pppppppp'}).save!
-          post :create, 'user' => {'email' => 'example@example.com', 'password' => '12345'}
-          expect(flash[:alert]).to have_content "I'm sorry, you are not authorized to login to the system."
-        end
-      end
-    end
-
-
   end
 end
