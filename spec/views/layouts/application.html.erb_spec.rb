7d46561712295697ebb4696ab3721225c055baa0
Sam Joseph <tansaku@gmail.com>
2014-03-19 14:14:42 +0000

    refactored test to make stronger, implemented dynamic links and patched various other tests that make use of the layout

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,156 +1,182 @@
 require 'spec_helper'
 
 describe "layouts/application.html.erb", :type => :feature do
-  context "no user signed-in" do
-
+  context 'everything' do
     before :each do
-      view.stub :user_signed_in? => false
+      Page.stub(:all).and_return []
     end
-    it "renders site title" do
-      render
-      rendered.should contain 'Harrow Community Network'
+    context "no user signed-in" do
+
+      before :each do
+        view.stub :user_signed_in? => false
+      end
+      it "renders site title" do
+        render
+        rendered.should contain 'Harrow Community Network'
+      end
+
+      it "renders site tag line" do
+        render
+        rendered.should contain 'Search for local voluntary and community organisations'
+      end
+
+      it 'renders Organization sign in form' do
+        render
+        rendered.should have_selector("form", :id => "loginForm")
+      end
+
+      it 'renders sign in form fields correctly' do
+        render
+        rendered.should have_css("#loginForm input#user_password")
+        rendered.should have_css("#loginForm input#user_email")
+        rendered.should have_css("#loginForm input#signin")
+      end
+
+      it 'renders dropdown menu' do
+        render
+        rendered.should have_css("#navLogin")
+      end
+
+      it "renders new Organization sign up form" do
+        render
+        rendered.should have_selector("form", :id => "registerForm")
+      end
+
+      it 'renders sign up form fields correctly' do
+        render
+        rendered.should have_css("#registerForm input#signup_password")
+        rendered.should have_css("#registerForm input#signup_email")
+        rendered.should have_css("#registerForm input#signup_password_confirmation")
+        rendered.should have_css("#registerForm div input[value=\"Sign up\"]")
+      end
+
+      it 'renders a password retrieval link' do
+        #<a href="/users/password/new">Forgot your password?</a>
+        render
+        rendered.should have_css("#menuLogin a[href=\"#{new_user_password_path}\"]")
+      end
+
+      it 'renders a cookies choice message when cookies have not been accepted' do
+        view.stub(:cookies_accepted?).and_return(false)
+        render
+        rendered.should have_css("#cookie-message")
+        rendered.should have_css("#cookie-message-inner.content")
+        rendered.should have_content("We use cookies to give you the best experience on our website.")
+        rendered.should have_xpath("//a[@id=\"accept_cookies\"]")
+        rendered.should have_xpath("//a[@href=\"#{cookies_allow_path}\"]")
+      end
+
+      it 'login form should be visible', :js => true do
+        render
+        rendered.should_not have_selector("form#loginForm", style: "height: 0px;")
+      end
+
+      it 'should not have any flash messages' do
+        render
+        rendered.should_not have_selector("div.alert")
+      end
+
+      it 'should display flash messages when successful' do
+        view.stub(:flash).and_return([[:notice, "Yes, we have been successful!!!!!"]])
+        render
+        rendered.should have_selector("div.alert")
+        rendered.should have_content("Yes, we have been successful!!!!!")
+        rendered.should have_selector("div.alert-success")
+      end
+
+      it 'should display flash messages when failing' do
+        view.stub(:flash).and_return([[:error, "No, no, no!"]])
+        render
+        rendered.should have_selector("div.alert")
+        rendered.should have_content("No, no, no!")
+        rendered.should have_selector("div.alert-error")
+      end
+
+      it 'should display a logo linked to the contributors page' do
+        render
+        doc = Nokogiri::HTML(rendered)
+        doc.xpath("//a/img[@alt='Agile Ventures Local Support']/..").first['href'].should eq contributors_path
+      end
+
+      it "does not render a new organization link" do
+        view.stub(:user_signed_in? => false)
+        render
+        rendered.should_not have_xpath("//a[@href='#{new_organization_path}']")
+      end
+
+      context 'footer links to pages' do
+
+        let(:page_one) { double(:page, :name => "About Us", :permalink => "about") }
+        let(:page_two) { double(:page, :name => "Contact", :permalink => "contact") }
+        let(:page_three) { double(:page, :name => "Disclaimer", :permalink => "disclaimer") }
+
+        it "shows no links to pages when there are no pages" do
+          absent_pages = [page_one, page_two, page_three]
+          render
+          absent_pages.each do |page|
+            rendered.should_not have_link(page.name, :href => page_path(page.permalink))
+          end
+        end
+
+        it "shows a link to all of the editable pages" do
+          pages = [page_one, page_two]
+          absent_pages = [page_three]
+          Page.should_receive(:all).and_return pages
+          render
+          pages.each do |page|
+            rendered.should have_link(page.name, :href => page_path(page.permalink))
+          end
+          absent_pages.each do |page|
+            rendered.should_not have_link(page.name, :href => page_path(page.permalink))
+          end
+        end
+      end
+
     end
 
-    it "renders site tag line" do
-      render
-      rendered.should contain 'Search for local voluntary and community organisations'
+    context "regular user signed-in" do
+      before :each do
+        view.stub :user_signed_in? => true
+        @user = double(User, email: 'normal_user@example.com', admin?: false)
+        view.stub :current_user => @user
+      end
+      it 'renders signed in users email' do
+        render
+        rendered.should have_link('normal_user@example.com')
+      end
+
+      it 'contains log out link' do
+        render
+        rendered.should have_css("li.dropdown ul.dropdown-menu li a[href=\"#{destroy_user_session_path}\"]")
+      end
+
+      it 'admin-only buttons: Organizations and Users' do
+        render
+        rendered.should_not have_css('.menuOrgs')
+        rendered.should_not have_css('.menuUsers')
+        @user.stub :admin? => true
+        render
+        rendered.should have_css('#menuOrgs')
+        rendered.should have_css('#menuUsers')
+      end
+
+      it "does not render a new organization link" do
+        render
+        rendered.should_not have_xpath("//a[@href='#{new_organization_path}']")
+      end
     end
 
-    it 'renders Organization sign in form' do
-      render
-      rendered.should have_selector("form", :id => "loginForm")
-    end
+    context "admin signed-in" do
+      it 'links to new org link in footer' do
+        view.stub(:user_signed_in?).and_return(true)
+        user = double(User)
+        user.stub(:email).and_return('normal_user@example.com')
+        user.stub(:admin?).and_return(true)
+        view.stub(:current_user).and_return(user)
+        render
 
-    it 'renders sign in form fields correctly' do
-       render
-       rendered.should have_css("#loginForm input#user_password")
-       rendered.should have_css("#loginForm input#user_email")
-       rendered.should have_css("#loginForm input#signin")
-    end
-
-    it 'renders dropdown menu' do
-      render
-      rendered.should have_css("#navLogin")
-    end
-
-    it "renders new Organization sign up form" do
-      render
-      rendered.should have_selector("form", :id => "registerForm")
-    end
-
-    it 'renders sign up form fields correctly' do
-      render
-      rendered.should have_css("#registerForm input#signup_password")
-      rendered.should have_css("#registerForm input#signup_email")
-      rendered.should have_css("#registerForm input#signup_password_confirmation")
-      rendered.should have_css("#registerForm div input[value=\"Sign up\"]")
-    end
-
-    it 'renders a password retrieval link' do
-      #<a href="/users/password/new">Forgot your password?</a>
-      render
-      rendered.should have_css("#menuLogin a[href=\"#{new_user_password_path}\"]")
-    end
-
-    it 'renders a cookies choice message when cookies have not been accepted' do
-      view.stub(:cookies_accepted?).and_return(false)
-      render
-      rendered.should have_css("#cookie-message")
-      rendered.should have_css("#cookie-message-inner.content")
-      rendered.should have_content("We use cookies to give you the best experience on our website.")
-      rendered.should have_xpath("//a[@id=\"accept_cookies\"]")
-      rendered.should have_xpath("//a[@href=\"#{cookies_allow_path}\"]")
-    end
-
-    it 'login form should be visible', :js => true do
-      render
-      rendered.should_not have_selector("form#loginForm", style: "height: 0px;")
-    end
-
-    it 'should not have any flash messages' do
-      render
-      rendered.should_not have_selector("div.alert")
-    end
-
-    it 'should display flash messages when successful' do
-      view.stub(:flash).and_return([[:notice,"Yes, we have been successful!!!!!"]])
-      render
-      rendered.should have_selector("div.alert")
-      rendered.should have_content("Yes, we have been successful!!!!!")
-      rendered.should have_selector("div.alert-success")
-    end
-
-    it 'should display flash messages when failing' do
-      view.stub(:flash).and_return([[:error,"No, no, no!"]])
-      render
-      rendered.should have_selector("div.alert")
-      rendered.should have_content("No, no, no!")
-      rendered.should have_selector("div.alert-error")
-    end
-
-     it 'should display a logo linked to the contributors page' do
-      render
-      doc = Nokogiri::HTML(rendered)
-      doc.xpath("//a/img[@alt='Agile Ventures Local Support']/..").first['href'].should eq contributors_path
-     end
-
-    it "does not render a new organization link"  do
-      view.stub(:user_signed_in? => false)
-      render
-      rendered.should_not have_xpath("//a[@href='#{new_organization_path}']")
-    end
-
-    it "shows a link to all of the editable pages" do
-      page_one = double(:page, :name => "About AV", :permalink => "about")
-      view.stub(:pages => [page_one])
-      render
-      rendered.should have_link('About AV', :href => '/about')
-    end
-
-  end
-
-  context "regular user signed-in" do
-    before :each do
-      view.stub :user_signed_in? => true
-      @user = double(User, email: 'normal_user@example.com', admin?: false)
-      view.stub :current_user => @user
-    end
-    it 'renders signed in users email' do
-      render
-      rendered.should have_link('normal_user@example.com')
-    end
-
-    it 'contains log out link' do
-      render
-      rendered.should have_css("li.dropdown ul.dropdown-menu li a[href=\"#{destroy_user_session_path}\"]")
-    end
-
-    it 'admin-only buttons: Organizations and Users' do
-      render
-      rendered.should_not have_css('.menuOrgs')
-      rendered.should_not have_css('.menuUsers')
-      @user.stub :admin? => true
-      render
-      rendered.should have_css('#menuOrgs')
-      rendered.should have_css('#menuUsers')
-    end
-
-    it "does not render a new organization link"  do
-      render
-      rendered.should_not have_xpath("//a[@href='#{new_organization_path}']")
-    end
-  end
-
-  context "admin signed-in" do
-    it 'links to new org link in footer' do
-      view.stub(:user_signed_in?).and_return(true)
-      user = double(User)
-      user.stub(:email).and_return('normal_user@example.com')
-      user.stub(:admin?).and_return(true)
-      view.stub(:current_user).and_return(user)
-      render
-
-      rendered.should have_link("New Organisation",href: new_organization_path)
+        rendered.should have_link("New Organisation", href: new_organization_path)
+      end
     end
   end
 end
--- spec/views/organizations/index.html.erb_spec.rb
+++ spec/views/organizations/index.html.erb_spec.rb
@@ -65,6 +65,7 @@ describe "organizations/index.html.erb", :js => true do
 
   it "displays the javascript for a google map" do
     assign(:json, organizations.to_gmaps4rails)
+    Page.stub(:all).and_return []
     render template: "organizations/index", layout: "layouts/application"
     rendered.should have_xpath "//script[contains(.,'Gmaps.map.map_options.auto_adjust = false')]"
     rendered.should have_xpath "//script[contains(.,'Gmaps.map.map_options.auto_zoom = true')]"
