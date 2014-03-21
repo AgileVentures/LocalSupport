ee720ce1588f9f1529a47f6a68f2346d4a14e7fe
johnnymo87 <johnnymo87@gmail.com>
2014-03-21 02:04:46 +0000

    removed accidental file, fixed view spec to handle time_ago_in_words

--- app/services/list_invited_users.rb
+++ app/services/list_invited_users.rb
@@ -10,7 +10,7 @@ class ListInvitedUsers
 
   def list 
     users.each_with_object([]) do |user, array|
-      org = org(user.email)
+      org = organization_class.find_by_email(user.email)
       array << build_invited_if_organization_exists(org, user) if org.present?
     end
   end
@@ -23,10 +23,6 @@ class ListInvitedUsers
       name: org.name, 
       email: org.email, 
       date: user.invitation_sent_at }
-  end
-
-  def org(user_email)
-    organization_class.find_by_email(user_email)
   end
 
 end
--- spec/views/user_reports/invited.html.erb_spec.rb
+++ spec/views/user_reports/invited.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'user_reports/invited.html.erb' do
-  let(:invitation) { { id: '2', name: 'Harrow Charity', email: 'hello@there.com', date: '8 days' } }
+  let(:invitation) { { id: '2', name: 'Harrow Charity', email: 'hello@there.com', date: Time.now } }
   before(:each) { assign(:invitations, [invitation]) }
 
   it 'columns: org name, email, date, checkbox' do
@@ -9,7 +9,7 @@ describe 'user_reports/invited.html.erb' do
     rendered.within("##{invitation[:id]}") do |row|
       row.should have_link invitation[:name], :href => organization_path(invitation[:id])
       row.should have_text invitation[:email]
-      row.should have_text invitation[:date]
+      row.should have_text 'less than a minute'
       row.should have_css('input[type=checkbox]')
     end
   end
