0a7c9ea1b912dc7fb397b269f268be423a4c3457
johnnymo87 <johnnymo87@gmail.com>
2014-03-26 19:00:06 +0000

    expected to find css div[data-resend_invitation=true] but there were no matches

--- spec/views/layouts/invitation_table.html.erb_spec.rb
+++ spec/views/layouts/invitation_table.html.erb_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe 'layouts/invitation_table.html.erb' do
+  before(:each) { render }
   it 'toolbar' do
-    render
     rendered.within('#toolbar') do |toolbar|
       toolbar.should have_button 'Invite Users'
       toolbar.should have_button 'Select All'
     end
   end
   it 'well' do
-    render
     rendered.should have_css('div[class=well]')
+  end
+  it 'resend invitation variable' do
+    rendered.should have_css('div[data-resend_invitation=true]')
   end
 end
