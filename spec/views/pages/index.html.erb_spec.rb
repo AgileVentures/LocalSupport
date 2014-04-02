5820a58e7fcd5e8bdc625ec3c48d058b44ee0679
johnnymo87 <johnnymo87@gmail.com>
2014-04-02 13:25:39 +0100

    expected to find link 'Hide Link' but there were no matches

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -1,8 +1,40 @@
 require 'spec_helper'
 
 describe "pages/index.html.erb" do
+  let(:page) { mock_model Page,
+                      {
+                          id: 1,
+                          name: 'About',
+                          permalink: 'about',
+                          content: 'blah blah',
+                          created_at: 'Y2K',
+                          link_visible: true
+                      }
+  }
+  before(:each) { assign(:pages, [page]) }
 
-  it "has a Hide Link button for each page" do
-    pending
+  context 'Page is shown' do
+
+    it 'has buttons' do
+      render
+      rendered.within('tbody tr') do |row|
+        row.should have_link 'Edit'
+        row.should have_link 'Delete'
+        row.should have_link 'Hide Link'
+      end
+    end
+  end
+
+  context 'Page is hidden' do
+    before(:each) { page.stub :link_visible => false }
+
+    it 'has buttons' do
+      render
+      rendered.within('tbody tr') do |row|
+        row.should have_link 'Edit'
+        row.should have_link 'Delete'
+        row.should have_link 'Show Link'
+      end
+    end
   end
 end
