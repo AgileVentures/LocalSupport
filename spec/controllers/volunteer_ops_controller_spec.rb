2ac95a0870dc48d449deea0bec2dac7bb99fa911
johnnymo87 <johnnymo87@gmail.com>
2014-04-10 18:30:08 +0100

    create action specs

--- spec/controllers/volunteer_ops_controller_spec.rb
+++ spec/controllers/volunteer_ops_controller_spec.rb
@@ -9,18 +9,21 @@ describe VolunteerOpsController do
     op.stub organization: org
     # ops_collection.stub find: op
   end
-  
+
   describe 'GET index' do
     before { VolunteerOp.stub all: ops_collection }
+
     it 'assigns all volunteer_ops as @volunteer_ops' do
       get :index, {}
       assigns(:volunteer_ops).should eq ops_collection
     end
+
     it 'non-org-owners allowed' do
       controller.stub current_user: user, org_owner?: false
       get :index, {}
       response.status.should eq 200
     end
+
     it 'mutation-proofing' do
       VolunteerOp.should_receive(:all)
       get :index, {}
@@ -29,15 +32,18 @@ describe VolunteerOpsController do
 
   describe 'GET show' do
     before { VolunteerOp.stub find: op }
+
     it 'assigns the requested volunteer_op as @volunteer_op' do
       get :show, {:id => op.id}
       assigns(:volunteer_op).should eq(op)
     end
+
     it 'non-org-owners allowed' do
       controller.stub current_user: user, org_owner?: false
       get :show, {:id => op.id}
       response.status.should eq 200
     end
+
     it 'mutation-proofing' do
       VolunteerOp.should_receive(:find).with(op.id)
       get :show, {:id => op.id}
@@ -49,15 +55,18 @@ describe VolunteerOpsController do
       controller.stub current_user: user, org_owner?: true
       VolunteerOp.stub new: op
     end
+
     it 'assigns the requested volunteer_op as @volunteer_op' do
       get :new, {}
       assigns(:volunteer_op).should eq op
     end
+
     it 'non-org-owners denied' do
       controller.stub org_owner?: false
       get :new, {}
       response.status.should eq 302
     end
+
     it 'mutation-proofing' do
       VolunteerOp.should_receive(:new)
       get :new, {}
@@ -69,15 +78,18 @@ describe VolunteerOpsController do
       controller.stub current_user: user, org_owner?: true
       VolunteerOp.stub find: op
     end
+
     it 'assigns the requested volunteer_op as @volunteer_op' do
       get :edit, {:id => op.id}
       assigns(:volunteer_op).should eq op
     end
+
     it 'non-org-owners denied' do
       controller.stub org_owner?: false
       get :edit, {:id => op.id}
       response.status.should eq 302
     end
+
     it 'mutation-proofing' do
       VolunteerOp.should_receive(:find).with(op.id)
       get :edit, {:id => op.id}
@@ -85,40 +97,48 @@ describe VolunteerOpsController do
   end
 
   describe 'POST create' do
-    describe 'with valid params' do
-      it 'creates a new VolunteerOp' do
-        expect {
-          post :create, {:volunteer_op => valid_attributes}, valid_session
-        }.to change(VolunteerOp, :count).by(1)
-      end
-
-      it 'assigns a newly created volunteer_op as @volunteer_op' do
-        post :create, {:volunteer_op => valid_attributes}, valid_session
-        assigns(:volunteer_op).should be_a(VolunteerOp)
-        assigns(:volunteer_op).should be_persisted
-      end
-
-      it 'redirects to the created volunteer_op' do
-        post :create, {:volunteer_op => valid_attributes}, valid_session
-        response.should redirect_to(VolunteerOp.last)
-      end
+    let(:op) { stub_model VolunteerOp, id: '9' }
+    let(:attributes) { {title: 'hard work', description: 'for the willing'} }
+    before do
+      controller.stub current_user: user, org_owner?: true
+      VolunteerOp.stub new: op
+      op.stub save: true
     end
 
-    describe 'with invalid params' do
-      it 'assigns a newly created but unsaved volunteer_op as @volunteer_op' do
-        # Trigger the behavior that occurs when invalid params are submitted
-        VolunteerOp.any_instance.stub(:save).and_return(false)
-        post :create, {:volunteer_op => { 'title' => 'invalid value' }}, valid_session
-        assigns(:volunteer_op).should be_a_new(VolunteerOp)
-      end
-
-      it 're-renders the "new" template' do
-        # Trigger the behavior that occurs when invalid params are submitted
-        VolunteerOp.any_instance.stub(:save).and_return(false)
-        post :create, {:volunteer_op => { 'title' => 'invalid value' }}, valid_session
-        response.should render_template('new')
-      end
+    it 'assigns a newly created volunteer_op as @volunteer_op' do
+      post :create, {volunteer_op: attributes}
+      assigns(:volunteer_op).should eq op
     end
+
+    it 'non-org-owners denied' do
+      controller.stub org_owner?: false
+      post :create, {volunteer_op: attributes}
+      response.status.should eq 302
+    end
+
+    it 'mutation-proofing' do
+      VolunteerOp.should_receive(:new).with(attributes.stringify_keys) { op }
+      op.should_receive :save
+      post :create, {volunteer_op: attributes}
+    end
+
+    it 'redirects to the created volunteer_op' do
+      post :create, {volunteer_op: attributes}
+      response.should redirect_to(op)
+    end
+
+    it 'with invalid attributes, it re-renders the "new" template' do
+      op.stub save: false
+      post :create, {volunteer_op: attributes}
+      response.should render_template('new')
+    end
+
+    # TODO: Move to request spec
+    # it 'creates a new VolunteerOp' do
+    #   expect {
+    #     post :create, {:volunteer_op => valid_attributes}, valid_session
+    #   }.to change(VolunteerOp, :count).by(1)
+    # end
   end
 
   describe 'PUT update' do
@@ -129,8 +149,8 @@ describe VolunteerOpsController do
         # specifies that the VolunteerOp created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        VolunteerOp.any_instance.should_receive(:update_attributes).with({ 'title' => 'MyString' })
-        put :update, {:id => volunteer_op.to_param, :volunteer_op => { 'title' => 'MyString' }}, valid_session
+        VolunteerOp.any_instance.should_receive(:update_attributes).with({'title' => 'MyString'})
+        put :update, {:id => volunteer_op.to_param, :volunteer_op => {'title' => 'MyString'}}, valid_session
       end
 
       it 'assigns the requested volunteer_op as @volunteer_op' do
@@ -151,7 +171,7 @@ describe VolunteerOpsController do
         volunteer_op = VolunteerOp.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         VolunteerOp.any_instance.stub(:save).and_return(false)
-        put :update, {:id => volunteer_op.to_param, :volunteer_op => { 'title' => 'invalid value' }}, valid_session
+        put :update, {:id => volunteer_op.to_param, :volunteer_op => {'title' => 'invalid value'}}, valid_session
         assigns(:volunteer_op).should eq(volunteer_op)
       end
 
@@ -159,7 +179,7 @@ describe VolunteerOpsController do
         volunteer_op = VolunteerOp.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         VolunteerOp.any_instance.stub(:save).and_return(false)
-        put :update, {:id => volunteer_op.to_param, :volunteer_op => { 'title' => 'invalid value' }}, valid_session
+        put :update, {:id => volunteer_op.to_param, :volunteer_op => {'title' => 'invalid value'}}, valid_session
         response.should render_template('edit')
       end
     end
