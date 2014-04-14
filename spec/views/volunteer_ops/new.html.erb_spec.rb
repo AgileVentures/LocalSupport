7bd43d8b3756aede1a8ff0345fc1145ff82c165a
marianmosley <marian.mosley@gmail.com>
2014-04-14 11:41:36 -0700

    ehd

--- spec/views/volunteer_ops/new.html.erb_spec.rb
+++ spec/views/volunteer_ops/new.html.erb_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe "volunteer_ops/new" do
+  let (:organization) { double :organization, id: 3 } 
+  let (:user) { double :user, organization: organization } 
   before(:each) do
     assign(:volunteer_op, stub_model(VolunteerOp,
       :title => "MyString",
       :description => "MyText",
       :organization => nil
     ).as_new_record)
+    view.stub current_user: user
   end
 
   it "renders new volunteer_op form" do
@@ -15,7 +18,11 @@ describe "volunteer_ops/new" do
     rendered.should have_selector("form", :action => volunteer_ops_path, :method => "post") do |form|
       form.should have_selector("input#volunteer_op_title", :name => "volunteer_op[title]")
       form.should have_selector("textarea#volunteer_op_description", :name => "volunteer_op[description]")
-      form.should have_selector("input#volunteer_op_organization", :name => "volunteer_op[organization]")
     end
+  end
+  it "should have a Create Volunteer Opportunity button" do
+    render
+    debugger
+    rendered.should have_link "Create Volunteer Opportunity", :method => :post, :href => volunteer_ops_path(current_user.organization)
   end
 end
