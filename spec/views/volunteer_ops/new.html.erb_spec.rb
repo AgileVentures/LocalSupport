418861be67060cf32cf8f5c3846963bc2992b287
johnnymo87 <johnnymo87@gmail.com>
2014-04-15 16:51:43 +0100

    saving is hard

--- spec/views/volunteer_ops/new.html.erb_spec.rb
+++ spec/views/volunteer_ops/new.html.erb_spec.rb
@@ -20,8 +20,8 @@ describe "volunteer_ops/new" do
 
   it "renders new volunteer_op form" do
     render
-    rendered.should have_selector('h3')
 	rendered.should have_selector("form", :action => volunteer_ops_path(org_id: view.current_user.organization.id), :method => "post") do |form|
+
       form.should have_selector("input#volunteer_op_title", :name => "volunteer_op[title]")
       form.should have_selector("textarea#volunteer_op_description", :name => "volunteer_op[description]")
     end
@@ -29,7 +29,7 @@ describe "volunteer_ops/new" do
 
   it "should have a Create Volunteer Opportunity button" do
     render
-    rendered.should have_css 'input[value="Create Volunteer Opportunity"]'
+    rendered.should have_css 'input[value="Create a Volunteer Opportunity"]'
   end
 
   it "only has 1 text area and 1 text input" do
