e7fe1050178ffc306b04b21d24c7772b5c02f064
johnnymo87 <johnnymo87@gmail.com>
2014-04-26 07:19:24 +0100

    tweaked org index view spec

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -1,40 +1,9 @@
 require 'spec_helper'
 
-describe 'Organizations' do
-  describe 'organizations index' do
-    it 'renders an in-line script with settings used for interacting with client-side Google Maps API scripts' do
+describe "Organizations" do
+  describe "GET /organizations" do
+    it "works! (now write some real specs)" do
       get organizations_path
-      response.body.should have_xpath "//script[contains(.,'Gmaps.map.map_options.auto_adjust = false')]"
-      response.body.should have_xpath "//script[contains(.,'Gmaps.map.map_options.auto_zoom = true')]"
-      response.body.should have_xpath "//script[contains(.,'Gmaps.map.map_options.center_latitude = 51.5978')]"
-      response.body.should have_xpath "//script[contains(.,'Gmaps.map.map_options.center_longitude = -0.337')]"
-      response.body.should have_xpath "//script[contains(.,'Gmaps.map.map_options.zoom = 12')]"
-      response.body.should have_xpath "//script[contains(.,'Gmaps.map.map_options.auto_adjust = false')]"
     end
-  end
-end
-
-describe 'Geocoding Organizations' do
-  let(:pinner34) { FactoryGirl.create :organization, address: '34 pinner road', postcode: 'HA1 4HZ' }
-
-  it '' do
-
-    pinner34
-  end
-
-  it 'geocodes' do
-    pinner34.latitude.should_not be_nil
-    pinner34.longitude.should_not be_nil
-  end
-
-  it 'geocodes again if the address changes' do
-    lat, lng = pinner34.latitude, pinner34.longitude
-    pinner34.address = '84 pinner road'
-    pinner34.postcode = 'HA1 4HZ'
-    pinner34.save!
-    pinner34.latitude.should_not be_nil
-    pinner34.longitude.should_not be_nil
-    pinner34.latitude.should_not eq lat
-    pinner34.longitude.should_not eq lng
   end
 end
