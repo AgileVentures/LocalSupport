ac70dcda0352ee35a367a3e27b6475d9ed6f7a2c
johnnymo87 <johnnymo87@gmail.com>
2014-04-28 17:19:00 +0100

    which syntax

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 shared_examples_for 'a restricted API' do
-  @restricted.each do |route|
+  debugger
+  restricted.each do |route|
     it '#{route[:action]} is restricted' do
       eval("request_via_redirect(:#{route[:verb]}, '#{route[:url]}')")
     end
   end
-  @unrestricted.each do |route|
+  unrestricted.each do |route|
     it "#{route[:action]} is not restricted" do
       eval("request_via_redirect(:#{route[:verb]}, '#{route[:url]}')")
     end
@@ -14,11 +15,11 @@ shared_examples_for 'a restricted API' do
 end
 
 describe "Organizations", :helpers => :requests do
-  let(:org) { FactoryGirl.create :organization }
+  subject { FactoryGirl.create :organization }
 
   before do
     routes = RouteFinder.new(OrganizationsController)
-    routes.add_matcher({:id => org.id}, [:create, :show, :edit, :update, :destroy])
+    routes.add_matcher({:id => subject.id}, [:create, :show, :edit, :update, :destroy])
     routes.make_urls
     # routes.add_params({:organization => attributes_for(:organization)}, [:create, :update])
     @unrestricted = routes.find(:only => [:search, :index, :show])
@@ -31,6 +32,9 @@ describe "Organizations", :helpers => :requests do
   #   puts 'lo'
   # end
 
-  it_behaves_like 'a restricted API'
+  it_should_behave_like 'a restricted API' do
+    let(:restricted) { @restricted }
+    let(:unrestricted) { @unrestricted }
+  end
 
 end
