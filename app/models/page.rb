f55b7e4b58f60cba0673a5e84bfde86f7a0f2414
David Corking <dmc-source@dcorking.com>
2014-04-28 17:21:29 +0100

    spec Page::visible_links, failing

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,10 @@ class Page < ActiveRecord::Base
   validates_presence_of :name, :permalink
   validates_uniqueness_of :permalink
 
+  # Provides links for page footers
+  def self.visible_links
+  end
+
   # Override method to allow static pages to be found via permalink instead of id
   def to_param
     permalink
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -16,10 +16,31 @@ describe Page do
     @page.link_visible = false
     @page.link_visible.should eq false
   end
-  describe 'self.create!' do
-    it 'can set the link_visible attribute' do
-      pg = Page.create!({"name" => "MyString", "permalink" => "my_link", :link_visible => false })
-      pg.reload.link_visible.should eq false
+  describe '::create!' do
+    it 'can set the link_visible attribute to be false' do
+      unlinked_page = Page.create!({"name" => "MyString", "permalink" => "my_link", :link_visible => false })
+      unlinked_page.reload.link_visible.should eq false
+    end
+  end
+  describe '::visible_links' do
+    it 'returns a collection of links to the pages that have visible links' do
+      first_linked_page = stub_model(Page, 
+                                     :name => "About us",
+                                     :permalink => "about",
+                                     :link_visible => true)
+      second_linked_page = stub_model(Page, 
+                                      :name => "An interesting page",
+                                      :permalink => "interesting",
+                                      :link_visible => true)
+
+      unlinked_page = stub_model(Page, 
+                                 :name => "A boring page",
+                                 :permalink => nil,
+                                 :link_visible => false)
+      expect(Page.visible_links).to eq [{:name => "About us", 
+                                          :permalink => "about"},
+                                        {:name => "An interesting page",
+                                          :permalink => "interesting"}] 
     end
   end
 end
