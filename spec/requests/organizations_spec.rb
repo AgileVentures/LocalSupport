d317cb1e4588ee41a0cf16a5878ede477ee42e1c
johnnymo87 <johnnymo87@gmail.com>
2014-04-29 18:54:37 +0100

    now can add params

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
 
-shared_examples_for 'a restricted API' do
-  debugger
-  restricted.each do |route|
-    it '#{route[:action]} is restricted' do
-      eval("request_via_redirect(:#{route[:verb]}, '#{route[:url]}')")
-    end
-  end
-  unrestricted.each do |route|
-    it "#{route[:action]} is not restricted" do
-      eval("request_via_redirect(:#{route[:verb]}, '#{route[:url]}')")
+shared_examples_for 'a restricted API' do |restricted|
+  # debugger
+  restricted.each_pair do |action, route|
+    it "#{action} is restricted" do
+      login(admin)
+      route = RequestHelpers::Routable.new(org, route)
+      # debugger
+      self.send(:request_via_redirect, route.verb, route.url)
+      # eval("request_via_redirect(:#{route.verb}, '#{route.url}')")
+      response.status.should be 200
+      flash[:error].should be nil
     end
   end
 end
 
 describe "Organizations", :helpers => :requests do
-  subject { FactoryGirl.create :organization }
+  extend RequestHelpers
 
-  before do
-    routes = RouteFinder.new(OrganizationsController)
-    routes.add_matcher({:id => subject.id}, [:create, :show, :edit, :update, :destroy])
-    routes.make_urls
-    # routes.add_params({:organization => attributes_for(:organization)}, [:create, :update])
-    @unrestricted = routes.find(:only => [:search, :index, :show])
-    @restricted = routes.find(:only => [:new, :create, :edit, :update, :destroy])
-  end
+  # debugger
+
+  routes = RequestHelpers::RouteFinder.new(OrganizationsController)
+  routes.add_param({:organization => {:name => 'hello'}}, [:create, :update])
+  restricted = routes.find(:only => [:new, :create, :edit, :update, :destroy])
 
   # it 'routes' do
   #   puts 'hi'
@@ -32,9 +30,10 @@ describe "Organizations", :helpers => :requests do
   #   puts 'lo'
   # end
 
-  it_should_behave_like 'a restricted API' do
-    let(:restricted) { @restricted }
-    let(:unrestricted) { @unrestricted }
+  it_should_behave_like 'a restricted API', restricted do
+    let(:org) { FactoryGirl.create :organization }
+    # let(:non-admin) { FactoryGirl.create :user }
+    let(:admin) { FactoryGirl.create :admin_user}
   end
 
 end
--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -6,6 +6,7 @@ module ControllerHelpers
     controller.stub(:current_user).and_return(admin_user)
     return admin_user
   end
+
   def make_current_user_nonadmin
     nonadmin_user = double("User")
     nonadmin_user.stub(:admin?).and_return(false)
@@ -30,16 +31,21 @@ module RequestHelpers
     end
   end
 
-  class RouteFinder
-    include Rails.application.routes.url_helpers
+  # def RouteFinder(controller, options = {})
+  #   RouteFinder.new(controller, options = {})
+  # end
 
-    attr_reader :actions
+  class RouteFinder
+    # include Rails.application.routes.url_helpers
+
+    # attr_reader :actions
 
     def initialize(controller, options = {})
-      controller_name = controller.to_s.chomp('Controller').downcase
+      @controller_name = controller.to_s.chomp('Controller').downcase
+      @model_name = controller.to_s.chomp('Controller').singularize
 
       routes = Rails.application.routes.routes.select do |route|
-        route.defaults[:controller] == controller_name
+        route.defaults[:controller] == @controller_name
       end
 
       @options = options
@@ -47,29 +53,31 @@ module RequestHelpers
       @actions = routes.each_with_object({}) do |route, hsh|
         action = route.defaults[:action].to_sym
         verb = find_verb_from(route.verb)
-        hsh[action] = verb
-      end
-    end
-
-    def add_matcher(hsh, keys)
-      keys.each do |key|
-        @actions[key].merge! hsh
-      end
-    end
-
-    def make_urls
-      @actions.each do |action|
-        @actions[action][:url] = url_for({:only_path => true, :controller => @controller_name}.merge!(action))
+        param_keys = find_param_keys_from(route.parts)
+        hsh[action] = {
+            :verb => verb,
+            :controller => @controller_name,
+            :action => action,
+            :param_keys => param_keys
+        }
       end
     end
 
     def find(options)
       if options[:only]
-        @actions.select { |key, _| @options[:only].include? key }
+        @actions.select { |key, _| options[:only].include? key }
       elsif options[:except]
-        @actions.select { |key, _| @options[:except].exclude? key }
+        @actions.select { |key, _| options[:except].exclude? key }
       else
         @actions
+      end
+    end
+
+    def add_param(param_hash, action_list)
+      action_list.each do |action|
+        param_hash.each do |key, value|
+          @actions[action][key] = value
+        end
       end
     end
 
@@ -78,6 +86,44 @@ module RequestHelpers
     def find_verb_from(regex)
       actions = %w(GET POST PUT DELETE)
       actions.select { |a| a.match(regex) }.first.downcase
+    end
+
+    def find_param_keys_from(list)
+      list.reject! { |elem| elem == :format }
+    end
+  end
+
+  # def Routable(object, options = {})
+  #   Routable.new(object, options = {})
+  # end
+
+
+  class Routable
+    include Rails.application.routes.url_helpers
+
+    attr_reader :verb
+
+    def initialize(object, routing_info = {})
+      @object = object
+      @verb = routing_info.delete(:verb)
+      @param_keys = routing_info.delete(:param_keys)
+      @routing_info = routing_info
+
+      @param_keys.each do |key|
+        @routing_info[key] = @object.send(key)
+      end
+
+      # puts 'hi'
+      # debugger
+      # puts 'lo'
+
+      # options.each do |attribute, value|
+      #   instance_variable_set("@#{attribute}", value)
+      # end
+    end
+
+    def url
+      url_for({:only_path => true}.merge!(@routing_info))
     end
   end
 end
