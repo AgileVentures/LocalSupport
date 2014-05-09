640fcfa3405f16a76f1d50d95e2408bf25619aa2
Sampriti Panda <sampdingo@hotmail.com>
2014-05-09 17:32:44 +0530

    Adding Routing Specs for Volunteer Feature Flags

--- spec/routing/volunteer_ops_routing_spec.rb
+++ spec/routing/volunteer_ops_routing_spec.rb
@@ -2,34 +2,76 @@ require "spec_helper"
 
 describe VolunteerOpsController do
   describe "routing" do
+    context 'feature flag enabled' do
+      it "routes to #index" do
+        get("/volunteer_ops").should route_to("volunteer_ops#index")
+      end
 
-    it "routes to #index" do
-      get("/volunteer_ops").should route_to("volunteer_ops#index")
+      it "routes to #new" do
+        get("/volunteer_ops/new").should route_to("volunteer_ops#new")
+      end
+
+      it "routes to #show" do
+        get("/volunteer_ops/1").should route_to("volunteer_ops#show", :id => "1")
+      end
+
+      it "routes to #edit" do
+        get("/volunteer_ops/1/edit").should route_to("volunteer_ops#edit", :id => "1")
+      end
+
+      it "routes to #create" do
+        post("/volunteer_ops").should route_to("volunteer_ops#create")
+      end
+
+      it "routes to #update" do
+        put("/volunteer_ops/1").should route_to("volunteer_ops#update", :id => "1")
+      end
+
+      it "routes to #destroy" do
+        delete("/volunteer_ops/1").should route_to("volunteer_ops#destroy", :id => "1")
+      end
+    end
+  end
+
+  context 'feature flag disabled' do
+    before(:each) do
+      Feature.create!(name: 'volunteer_ops', active: false)
     end
 
-    it "routes to #new" do
-      get("/volunteer_ops/new").should route_to("volunteer_ops#new")
+    it "does not route to #index" do
+      # This is the only way of testing this route because of the way
+      # the pages routing is set up. This will always raise a
+      # ActiveRecord::RecordNotFound error unless their is a static page
+      # with the permalink "volunteer_ops".
+      expect(get("/volunteer_ops")).to route_to(
+                                           :controller => "pages",
+                                           :action => "show",
+                                           :id => "volunteer_ops"
+                                       )
     end
 
-    it "routes to #show" do
-      get("/volunteer_ops/1").should route_to("volunteer_ops#show", :id => "1")
+    it "does not route to #new" do
+      get("/volunteer_ops/new").should_not be_routable
     end
 
-    it "routes to #edit" do
-      get("/volunteer_ops/1/edit").should route_to("volunteer_ops#edit", :id => "1")
+    it "does not route to #show" do
+      get("/volunteer_ops/1").should_not be_routable
     end
 
-    it "routes to #create" do
-      post("/volunteer_ops").should route_to("volunteer_ops#create")
+    it "does not route to #edit" do
+      get("/volunteer_ops/1/edit").should_not be_routable
     end
 
-    it "routes to #update" do
-      put("/volunteer_ops/1").should route_to("volunteer_ops#update", :id => "1")
+    it "does not route to #create" do
+      post("/volunteer_ops").should_not be_routable
     end
 
-    it "routes to #destroy" do
-      delete("/volunteer_ops/1").should route_to("volunteer_ops#destroy", :id => "1")
+    it "does not route to #update" do
+      put("/volunteer_ops/1").should_not be_routable
     end
 
+    it "does not route to #destroy" do
+      delete("/volunteer_ops/1").should_not be_routable
+    end
   end
 end
