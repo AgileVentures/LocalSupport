894fd972ceae9f2e786a2da96438c1f6d676b72f
johnnymo87 <johnnymo87@gmail.com>
2014-05-09 06:11:35 +0100

    moved infobox js to js file, moved script calls to partial, applied partial to index & show for orgs & vol_ops

---
+++ app/assets/javascripts/infobox.js
@@ -0,0 +1,21 @@
+Gmaps.map.infobox = function (boxText) {
+    return {
+        content: boxText,
+        disableAutoPan: false,
+        maxWidth: 0,
+        pixelOffset: new google.maps.Size(-140, 0),
+        zIndex: null,
+        boxStyle: {
+            background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/tags/infobox/1.1.5/examples/tipbox.gif') no-repeat",
+            opacity: 0.85,
+            width: "280px"
+        },
+        closeBoxMargin: "10px 2px 2px 2px",
+        closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+        infoBoxClearance: new google.maps.Size(1, 1),
+        isHidden: false,
+        pane: "floatPane",
+        enableEventPropagation: false
+    }
+};
+
