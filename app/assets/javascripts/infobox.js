000a437f91e99275d92d51aeaac562b1e32ddee7
johnnymo87 <johnnymo87@gmail.com>
2014-05-09 17:18:16 +0100

    inverted arrow across x-axis

--- app/assets/javascripts/infobox.js
+++ app/assets/javascripts/infobox.js
@@ -7,7 +7,7 @@ Gmaps.map.infobox = function (boxText) {
         pixelOffset: new google.maps.Size(-140, 0),
         zIndex: null,
         boxStyle: {
-            background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/tags/infobox/1.1.5/examples/tipbox.gif') no-repeat",
+            background: "url('/assets/tipbox.gif') no-repeat",
             opacity: 0.85,
             width: "280px"
         },
