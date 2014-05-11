92d9e2ff74104b88b51c67f79d374c160abec200
Sampriti Panda <sampdingo@hotmail.com>
2014-05-11 11:22:44 +0530

    Revert "Fixing Jasmine Warning Message"
    This reverts commit db856b3677aaec79eba4a191941e4b157cf60aec.

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-ENV['USE_JASMINE_RAKE']='true'
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require *Rails.groups(:assets => %w(development test))
