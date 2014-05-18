4decbbb4eb52d5083a9ab0f7bdfa4949a3984c98
johnnymo87 <johnnymo87@gmail.com>
2014-05-18 06:34:20 +0100

    made change in data structure more clear in test

--- spec/services/invitations_spec.rb
+++ spec/services/invitations_spec.rb
@@ -4,16 +4,18 @@ describe Invitations do
   let(:key_mapper) { Invitations::KeyMapper }
   let(:inviter) { Invitations::Inviter }
 
-  let(:array_of_hashes) { [{1=>'a'}, {2=>'b'}] }
-
   let(:params) { double :params }
   let(:invited_by) { double :invited_by }
 
   subject { described_class.(params, invited_by) }
 
-  it 'calls to a key mapper and inviter service, then reduces the results' do
+  it 'reduces the results from an array of hashes to a hash' do
     expect(key_mapper).to receive(:call).with(params) { key_mapper }
-    expect(inviter).to receive(:call).with(key_mapper, invited_by) { array_of_hashes }
-    expect(subject).to eq({1=>'a', 2=>'b'})
+    expect(inviter).to receive(:call).with(key_mapper, invited_by) do
+      [{1=>'a'}, {2=>'b'}]
+    end
+    expect(subject).to eq(
+      {1=>'a', 2=>'b'}
+    )
   end
 end
