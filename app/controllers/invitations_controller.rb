57bd1fba87069418438011602186412fd4ff149e
johnnymo87 <johnnymo87@gmail.com>
2014-05-20 15:30:22 +0100

    starting to pull out of controller method

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,10 +3,27 @@ class InvitationsController < ApplicationController
 
   # xhr only
   def create
-    render :json => ::Invitations.(
-      params,
-      current_user,
-    ).to_json
+    flag = params.fetch(:resend_invitation).to_s == 'true' ? true : false
+    Devise.resend_invitation = flag
+    answers = send_invites_and_gather_answers(params.fetch(:invite_list))
+    render json: answers.reduce({}, :update).to_json
   end
 
+  def send_invites_and_gather_answers(invite_list)
+    invite_list.map do |invite|
+      user = invite_single_user(invite)
+      answer = if user.errors.any?
+                 user.errors.full_messages.map{|msg| "Error: #{msg}"}.join(' ')
+               else
+                 'Invited!'
+               end
+      {invite.fetch(:id) => answer}
+    end
+  end
+
+  def invite_single_user(invite_params)
+    User.invite!({email: invite_params.fetch(:email)}, current_user) do |user|
+      user.organization_id = invite_params.fetch(:id)
+    end
+  end
 end
