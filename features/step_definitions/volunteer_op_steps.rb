c4d0b4102c93d4dca1f1021d69eaee04773813ca
David Corking <dmc-source@dcorking.com>
2014-06-12 17:07:38 +0100

    Delete unused step definition for multiple feature flags

--- features/step_definitions/volunteer_op_steps.rb
+++ features/step_definitions/volunteer_op_steps.rb
@@ -11,9 +11,3 @@ Given(/^that the (.+) flag is (enabled|disabled)$/) do |feature, state|
     Feature.create!(name: feature, active: (state == 'enabled'))
   end
 end
-
-# Given /the following feature flags exist/ do |feature_flags_table|
-#   feature_flags_table.hashes.each do |feature_flag|
-#     Feature.create! feature_flag
-#   end
-# end
