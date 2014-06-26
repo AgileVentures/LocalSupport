daf38f9f1c4964c4824844f83466d5d58ef3353a
marianmosley <marian.mosley@gmail.com>
2014-06-26 14:11:45 -0700

    remove unrelated steps file and cosmetic changes

--- spec/models/volunteer_op_spec.rb
+++ spec/models/volunteer_op_spec.rb
@@ -9,7 +9,6 @@ describe VolunteerOp do
     VolunteerOp.new(:title => "Title").should_not be_valid
   end
 
-
   it "is not valid without a title" do
     VolunteerOp.new(:organization_id => "1").should_not be_valid
   end
