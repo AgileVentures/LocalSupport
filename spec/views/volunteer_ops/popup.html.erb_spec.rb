413f4de0494cce53458de428a8c4d26aeadbd57c
David Corking <dmc-source@dcorking.com>
2014-06-26 17:32:47 +0100

    Refine spec for volunteer ops map popup partial

--- spec/views/volunteer_ops/popup.html.erb_spec.rb
+++ spec/views/volunteer_ops/popup.html.erb_spec.rb
@@ -5,23 +5,29 @@ describe "volunteer_ops/_popup.html.erb" do
     stub_model Organization, :name => "Friendly Charity", :id => 1, 
       :description => 'This is an absurdly absurdly long but very fun description that will make you sick '
   end
-  let(:op) do
-    stub_model VolunteerOp, :title => "Friendly Volunteer", :organization => org, :id => 1, 
-      :description => 'Hello!  This is an absurdly absurdly long but very fun description that will make you sick '
+  let(:op1) do
+    stub_model VolunteerOp, :title => "Friendly Volunteer", :organization_id => org.id, :id => 1, 
+      :description => 'Friendly people wanted!  This is an absurdly absurdly long but very fun description that will make you sick '
+  end
+  let(:op2) do
+    stub_model VolunteerOp, :title => "Friendly Driver", :organization_id => org.id, :id => 1, 
+      :description => 'Drivers wanted!  This is an absurdly absurdly long but very fun description that will make you sick '
   end
 
   before(:each) do
-    assign(:op, op)
+    VolunteerOp.stub(:find_all_by_organization_id).and_return([op1, op2])
     assign(:org, org)
     render
   end
 
-  it "should render a link to a volunteer opportunity" do
-    expect(rendered).to have_link 'Friendly Volunteer', :href => volunteer_op_path(op)
+  it "should render a link to each volunteer opportunity at that org" do
+    expect(rendered).to have_link 'Friendly Volunteer', :href => volunteer_op_path(op1)
+    expect(rendered).to have_link 'Friendly Driver', :href => volunteer_op_path(op2)
   end
   
   it 'should render a description of a volunteer opportunity' do
-     expect(rendered).to have_content(smart_truncate(op.description, 32))
+     expect(rendered).to have_content(smart_truncate(op1.description, 32))
+     expect(rendered).to have_content(smart_truncate(op2.description, 32))
   end
   
   it "should render a link to an org" do
