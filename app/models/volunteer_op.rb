8c46f0894e8cc3468bc30f38deaccbe017c917c0
Sam Joseph <tansaku@gmail.com>
2014-07-07 16:57:36 +0100

    extra validation checking volunteer ops have orgs, and another high level feature to check non-org owners cant create volunteer ops

--- app/models/volunteer_op.rb
+++ app/models/volunteer_op.rb
@@ -1,5 +1,6 @@
 class VolunteerOp < ActiveRecord::Base
-  validates :title, :description, presence: true
+  validates :title, :description, :organization_id, presence: true
   belongs_to :organization
+
   attr_accessible :description, :title, :organization_id
 end
