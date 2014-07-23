2b2fa2674c86a305c80edcd590b9ea6f761639f0
marianmosley <marian.mosley@gmail.com>
2014-07-23 10:03:52 -0700

    add some space and rework the charity admin message

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,9 +1,10 @@
 module OrganizationsHelper
   def charity_admin_display_msg
     if @organization.users.empty?
-      "This organisation has no admins yet"
+      simple_format("\n\nThis organisation has no admins yet.")
     else
-      @organization.users.map {|user| user.email}.join(", ")
+      simple_format("\n\nThe following users are administrators for \n<strong>#{@organization.name}:</strong>
+      #{@organization.users.map {|user| user.email}.join("\n")}")
     end
   end
 end
