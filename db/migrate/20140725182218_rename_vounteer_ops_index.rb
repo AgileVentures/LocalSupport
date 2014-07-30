0a5631b34d3bff9f58513271776dcdcc016d9e50
marianmosley <marian.mosley@gmail.com>
2014-07-30 12:02:07 -0700

    make migration reversible

--- db/migrate/20140725182218_rename_vounteer_ops_index.rb
+++ db/migrate/20140725182218_rename_vounteer_ops_index.rb
@@ -1,3 +1,5 @@
 class RenameVounteerOpsIndex < ActiveRecord::Migration
-  rename_index :volunteer_ops, :index_volunteer_ops_on_organization_id, :index_volunteer_ops_on_organisation_id
+  def change
+    rename_index :volunteer_ops, :index_volunteer_ops_on_organization_id, :index_volunteer_ops_on_organisation_id
+  end
 end
