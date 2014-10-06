8335da82ed4aa4755264af6478dffe341259bbf0
Paul McCulloch <hrfeels@gmail.com>
2014-10-06 12:44:53 -0700

    removed db cleaner between runs, succeeds with "better" specs and before :each

--- spec/db_cleaner_spec.rb
+++ spec/db_cleaner_spec.rb
@@ -1,2 +1,19 @@
 require 'spec_helper'
-FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+
+describe 'database cleaner' do
+  # before :each aka before gets cleaned up, before :all does not
+  # if no examples to run, test is not executed
+  before :each do
+    FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+  end
+  it 'it block gets cleaned up' do
+    FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+  end
+  describe 'inner describe' do
+    it 'with it block cleaned up too' do
+      FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+    end
+  end
+end
+
+
