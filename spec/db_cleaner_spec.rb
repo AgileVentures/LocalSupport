75766b199863301a4091ce91ad61f9637ab8d51c
Paul McCulloch <hrfeels@gmail.com>
2014-10-06 13:25:56 -0700

    change before :each to before :all, should fail with name incl fiendish-all

--- spec/db_cleaner_spec.rb
+++ spec/db_cleaner_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe 'database cleaner' do
   # before :each aka before gets cleaned up, before :all does not
   # if no examples to run, test is not executed
-  before :each do
-    FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+  before :all do
+    FactoryGirl.create(:organisation, name: 'fiendish-all', address: nil, postcode: nil)
   end
   it 'it block gets cleaned up' do
-    FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+    FactoryGirl.create(:organisation, name: 'fiendish-it', address: nil, postcode: nil)
   end
   describe 'inner describe' do
     it 'with it block cleaned up too' do
-      FactoryGirl.create(:organisation, name: 'fiendish', address: nil, postcode: nil)
+      FactoryGirl.create(:organisation, name: 'fiendish-inner', address: nil, postcode: nil)
     end
   end
 end
