f9b36ecbab0f4085d0c15408afb4c59aa92296fc
mtc2013 <mtc2013opensource@gmail.com>
2014-10-15 20:19:42 -0400

    Refactor our starship operator for category_organisation model

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,31 +4,7 @@ module FormHelper
       organisation.category_organisations.build(:category => category)
     end
     organisation.category_organisations.sort! do |first, second|
-      first_is_what_they_do = first.category.charity_commission_id < 200
-      first_is_who_they_help = (first.category.charity_commission_id < 300) & (first.category.charity_commission_id > 199)
-      first_is_how_they_help = (first.category.charity_commission_id < 400) & (first.category.charity_commission_id > 299)
-      second_is_what_they_do = second.category.charity_commission_id < 200
-      second_is_who_they_help = (second.category.charity_commission_id < 300) & (second.category.charity_commission_id > 199)
-      second_is_how_they_help = (second.category.charity_commission_id < 400) & (second.category.charity_commission_id > 299)
-      if first_is_what_they_do & second_is_what_they_do
-        first.category.name <=> second.category.name
-      elsif first_is_what_they_do & second_is_how_they_help
-        -1
-      elsif first_is_what_they_do & second_is_who_they_help
-        -1
-      elsif first_is_who_they_help & second_is_what_they_do
-        1
-      elsif first_is_who_they_help & second_is_who_they_help
-        first.category.name <=> second.category.name
-      elsif first_is_who_they_help & second_is_how_they_help
-        -1
-      elsif first_is_how_they_help & second_is_what_they_do
-        1
-      elsif first_is_how_they_help & second_is_who_they_help
-        1
-      elsif first_is_how_they_help & second_is_how_they_help
-       first.category.name <=> second.category.name
-      end
+      first <=> second
     end
     organisation
   end
--- app/models/category_organisation.rb
+++ app/models/category_organisation.rb
@@ -2,4 +2,32 @@ class CategoryOrganisation < ActiveRecord::Base
   belongs_to :category
   belongs_to :organisation
   self.table_name = 'categories_organisations'
+
+  def <=> other
+    self_is_what_they_do = self.category.charity_commission_id < 200
+    self_is_who_they_help = (self.category.charity_commission_id < 300) & (self.category.charity_commission_id > 199)
+    self_is_how_they_help = (self.category.charity_commission_id < 400) & (self.category.charity_commission_id > 299)
+    other_is_what_they_do = other.category.charity_commission_id < 200
+    other_is_who_they_help = (other.category.charity_commission_id < 300) & (other.category.charity_commission_id > 199)
+    other_is_how_they_help = (other.category.charity_commission_id < 400) & (other.category.charity_commission_id > 299)
+    if self_is_what_they_do & other_is_what_they_do
+      self.category.name <=> other.category.name
+    elsif self_is_what_they_do & other_is_how_they_help
+      -1
+    elsif self_is_what_they_do & other_is_who_they_help
+      -1
+    elsif self_is_who_they_help & other_is_what_they_do
+      1
+    elsif self_is_who_they_help & other_is_who_they_help
+      self.category.name <=> other.category.name
+    elsif self_is_who_they_help & other_is_how_they_help
+      -1
+    elsif self_is_how_they_help & other_is_what_they_do
+      1
+    elsif self_is_how_they_help & other_is_who_they_help
+      1
+    elsif self_is_how_they_help & other_is_how_they_help
+      self.category.name <=> other.category.name
+    end
+  end
 end
