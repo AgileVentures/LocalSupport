cef38cd2922075c79b369b17f42281734cc2128d
johnnymo87 <johnnymo87@gmail.com>
2014-11-16 18:48:17 +0000

    go back to using provider hash for settings so that map doesn't jump around on load

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,12 @@
 $(function () {
   var handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'map_canvas'} }, function(){
+  handler.buildMap({ internal: {id: 'map_canvas'},
+    provider: {
+      zoom: 12,
+      center: new google.maps.LatLng(51.5978, -0.3370),
+    }}, function(){
     var marker_data = $('#marker_data').data().markers;
     var markers = handler.addMarkers(marker_data);
     handler.bounds.extendWith(markers);
-    handler.getMap().setZoom(12);
-    handler.getMap().setCenter(new google.maps.LatLng(51.5978, -0.3370));
   });
 });
