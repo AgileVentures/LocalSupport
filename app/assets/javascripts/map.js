e44c90e8077419a5061a03ecb6a34d9721652052
johnnymo87 <johnnymo87@gmail.com>
2014-11-16 18:41:54 +0000

    remove fitToBounds

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,7 +4,6 @@ $(function () {
     var marker_data = $('#marker_data').data().markers;
     var markers = handler.addMarkers(marker_data);
     handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
     handler.getMap().setZoom(12);
     handler.getMap().setZoom(new google.maps.LatLng(51.5978, -0.3370));
   });
