4ba50a12e547814758a82898ff9347a0821dd457
johnnymo87 <johnnymo87@gmail.com>
2014-11-16 21:52:11 +0000

    stubs local for org form spec, updates js & json tests in org index spec

--- spec/views/organisations/form.html.erb_spec.rb
+++ spec/views/organisations/form.html.erb_spec.rb
@@ -12,6 +12,7 @@ describe "organisations/_form.html.erb" do
     @category6 = FactoryGirl.create(:category, name: "rabbit", charity_commission_id: 304)
     @organisation.categories << [@category1, @category3]
     @organisation.save!
+    view.stub markers: 'my-markers'
     render
   end
   it "renders form partial even for empty Organisation" do
