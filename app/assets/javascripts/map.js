1c1c405ca75c796735ae1d3031de44035b98bfdd
johnnymo87 <johnnymo87@gmail.com>
2014-11-16 18:38:43 +0000

    new way of setting center

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,16 +1,11 @@
 $(function () {
   var handler = Gmaps.build('Google');
-  handler.buildMap({
-    internal: {id: 'map_canvas'},
-    provider: {
-      center: new google.maps.LatLng(51.5978, -0.3370),
-      mapTypeId : google.maps.MapTypeId.ROADMAP,
-    }
-    }, function(){
+  handler.buildMap({ internal: {id: 'map_canvas'} }, function(){
     var marker_data = $('#marker_data').data().markers;
     var markers = handler.addMarkers(marker_data);
     handler.bounds.extendWith(markers);
     handler.fitMapToBounds();
     handler.getMap().setZoom(12);
+    handler.getMap().setZoom(new google.maps.LatLng(51.5978, -0.3370));
   });
 });
