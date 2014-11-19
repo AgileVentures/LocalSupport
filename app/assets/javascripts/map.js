4109347372e1ae54cb32d9c627b7d72acca2641d
mtc2013 <mtc2013opensource@gmail.com>
2014-11-19 09:17:31 -0500

    Revert changes with map.js and spec

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,17 +1,13 @@
-g = 
-  function (handler) {
-    return function(){
+$(function(){
+      var handler = Gmaps.build('Google');
       handler.buildMap({ internal: {id: 'map_canvas'},
       provider: {
         zoom: 12,
         center: new google.maps.LatLng(51.5978, -0.3370),
       }}, function(){
-      var marker_data = $('#marker_data').data().markers;
-      var markers = handler.addMarkers(marker_data);
-      handler.bounds.extendWith(markers);
-    });
-    }
-  }
+        var marker_data = $('#marker_data').data().markers;
+        var markers = handler.addMarkers(marker_data);
+        handler.bounds.extendWith(markers);
+      });
+});
 
-var handler = Gmaps.build('Google');
-//$(g(handler));
