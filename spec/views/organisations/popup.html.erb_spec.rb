2add62f8020397b7d5e136a150233d653dc2a4fa
johnnymo87 <johnnymo87@gmail.com>
2014-11-23 01:31:03 +0000

    remove lazy stub, actually pass down a local

--- spec/views/organisations/popup.html.erb_spec.rb
+++ spec/views/organisations/popup.html.erb_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 describe "organisations/_popup.html.erb", :type => :view do
   let(:org) do
-    stub_model Organisation, :name => "Friendly Charity", :id => 1, 
+    stub_model Organisation, :name => "Friendly Charity", :id => 1,
       :description => 'This is an absurdly absurdly long but very fun description that will make you sick '
   end
 
   before(:each) do
-    allow(view).to receive_messages org: org
-    render
+    render partial: "popup.html.erb", locals: { org: org }
   end
 
   it "should render a link to an org" do
     expect(rendered).to have_link 'Friendly Charity', :href => organisation_path(org)
   end
-  
+
   it 'should render a description of org' do
      expect(rendered).to have_content(smart_truncate(org.description, 32))
   end
