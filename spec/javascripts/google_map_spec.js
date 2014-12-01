167778249765d5a5addd6ae23229584438205acf
johnnymo87 <johnnymo87@gmail.com>
2014-12-01 04:44:56 +0000

    using done() to load lib before executing specs

--- spec/javascripts/google_map_spec.js
+++ spec/javascripts/google_map_spec.js
@@ -2,27 +2,28 @@
 
 
 describe('LocalSupport.GoogleMap', function() {
+  var settings;
 
-  beforeEach(function() {
+  beforeEach(function(done) {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
+    setFixtures(sandbox({
+      id: 'marker_data',
+      'data-markers': 'my-markers',
+    }));
+    appendSetFixtures(sandbox({ id: 'map_canvas' }))
+
+    GoogleMapsLoader.LIBRARIES = ['geometry'];
+    GoogleMapsLoader.load(function(google) {
+      settings = LocalSupport.MapSettings().for(google);
+      done();
+    });
   });
 
   it('expresses default settings in terms of internal and provider objects', function() {
-    GoogleMapsLoader.LIBRARIES = ['geometry', 'places'];
-    GoogleMapsLoader.load(function(google) {
-      setFixtures(sandbox({
-        id: 'marker_data',
-        'data-markers': 'my-markers',
-      }));
-      appendSetFixtures(sandbox({ id: 'map_canvas' }))
-      var settings = LocalSupport.MapSettings().for(google);
-      debugger
-      LocalSupport.GoogleMap().build(settings);
-    });
-    // setTimeout(function() {
-    //   done();
-    // }, 9000);
+    debugger
+    LocalSupport.GoogleMap().build(settings);
   });
 
   afterEach(function() {
