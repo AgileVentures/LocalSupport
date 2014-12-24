9ac1556f573357b471e1543f9104c4470356d162
mtc2013 <mtc2013opensource@gmail.com>
2014-12-24 13:31:45 -0500

    Finish happy path testing

--- features/step_definitions/organisation_reports/undo_user_delete_steps.rb
+++ features/step_definitions/organisation_reports/undo_user_delete_steps.rb
@@ -1,9 +1,13 @@
-Then(/^I restore the user with the email "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I restore the user with the email "(.*?)"$/) do |usr_email|
+  id = User.only_deleted.where(email: usr_email).first.id
+  within("tr##{id}") do
+    click_link "Restore User"
+  end
 end
 
-Then(/^the user with email "(.*?)" should be displayed on the all deleted users page$/) do |usr_email|
+Then(/^the user with email "(.*?)" should( not)? be displayed on the all deleted users page$/) do |usr_email, negation|
   visit deleted_users_report_path
-  id = User.only_deleted.where(email: usr_email).first.id
-  expect(page).to have_css "tr##{id}"
+  id = User.with_deleted.where(email: usr_email).first.id
+  assertion = negation ? :not_to : :to
+  expect(page).send(assertion, have_css("tr##{id}"))
 end
