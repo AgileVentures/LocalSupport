b3d3657869ab35dc573d35d2a48c2dcc48f82dee
mtc2013 <mtc2013opensource@gmail.com>
2015-01-12 11:42:37 -0500

    Move button into table to center it Provide 5 pixel top padding to row for button

--- app/controllers/proposed_organisation_edits_controller.rb
+++ app/controllers/proposed_organisation_edits_controller.rb
@@ -6,7 +6,8 @@ class ProposedOrganisationEditsController < ApplicationController
   end
   def create
     org = Organisation.find(params[:organisation_id])
-    create_params = params.require(:proposed_organisation_edit).permit(:address, :telephone, :postcode, :name, :description, :website, :postcode, :email, :donation_info).merge(organisation: org)
+    create_params = params.require(:proposed_organisation_edit).permit(:address, :telephone, :postcode, :name,
+      :description, :website, :postcode, :email, :donation_info).merge(organisation: org)
     redirect_to organisation_proposed_organisation_edit_path org, ProposedOrganisationEdit.create!(create_params)
   end
   def show
