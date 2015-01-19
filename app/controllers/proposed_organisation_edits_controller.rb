ac34ba6b3ed5be42adf0bdd1de5cf9c50305b96b
johnnymo87 <johnnymo87@gmail.com>
2015-01-19 18:55:20 +0000

    psuedo code of plan for accepted and archived bool columns

--- app/controllers/proposed_organisation_edits_controller.rb
+++ app/controllers/proposed_organisation_edits_controller.rb
@@ -17,5 +17,28 @@ class ProposedOrganisationEditsController < ApplicationController
   def index
     @proposed_organisation_edits = ProposedOrganisationEdit.all
   end
+  def update
+    if proposed_edit_params.any?
+      # update bool for accept / reject
+      @proposed_organisation_edit.accept_edits(propsed_edit_params)
+    end
+    # update bool for archived
+  end
+
+  def propsed_edit_params
+  end
+
+  #  +-----+-----+
+  #  |  1  |  2  |
+  #  |     |     |
+  #  +-----+-----+
+  #  |  3  |  4  |
+  #  |     |     |
+  #  +-----+-----+
+  #
+  #  1. Not archived, not accepted ... what Rachel sees in "All proposed edits"
+  #  2. Not archived, accepted ... invalid state
+  #  3. archived, not accepted ... rejected edits
+  #  4. archived, accepted ... accepted edits
 
 end
