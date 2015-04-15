5829e05f6f480f451e03ee280d9c5bbf7352e4ee
mtc2013 <mtc2013opensource@gmail.com>
2015-04-15 15:35:47 -0400

    Consolidate and dry out selected category steps

--- features/step_definitions/category_steps.rb
+++ features/step_definitions/category_steps.rb
@@ -18,19 +18,19 @@ Then(/^I uncheck the category "(.*?)"$/) do |category_name|
   end
 end
 
-Then(/^the "(.*?)" category should be selected from What They Do$/) do |name|
+Then(/^the "(.*?)" category should be selected from (What They Do|How They Help|Who They Help)$/) do |name, cat_type|
+  cat_id = case cat_type
+    when 'What They Do'
+      '#what_id'
+    when 'How They Help'
+      '#how_id'
+    when 'Who They Help'
+      '#who_id'
+    else
+      raise "Unsupported category type"
+  end
   id = Category.find_by(name: name).id.to_s
-  find("#what_id").value.should eq id
-end
-
-Then(/^the "(.*?)" category should be selected from How They Help$/) do |name|
-  id = Category.find_by(name: name).id.to_s
-  find("#how_id").value.should eq id
-end
-
-Then(/^the "(.*?)" category should be selected from Who They Help$/) do |name|
-  id = Category.find_by(name: name).id.to_s
-  find("#who_id").value.should eq id
+  find(cat_id).value.should eq id
 end
 
 Then(/^the default all value should be selected from What They Do$/) do
