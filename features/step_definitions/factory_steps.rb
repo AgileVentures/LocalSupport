16a64d0aa45d477f856f5f0e106fd56922e4541a
mtc2013 <mtc2013opensource@gmail.com>
2015-05-17 17:11:18 -0400

    Remove extra function defined in test api Change address to lower case to find fixtuers on travis

--- features/step_definitions/factory_steps.rb
+++ features/step_definitions/factory_steps.rb
@@ -1,14 +1,5 @@
 require_relative 'proposed_organisation_testing_api'
 
-def unsaved_proposed_organisation(associated_user = nil)
-  proposed_org = ProposedOrganisation.new({name: "Friendly Chariity", description: "We are friendly!", 
-    email: "sample@sample.org", address: "30 Pinner Road", donation_info: "https://www.donate.com",
-    postcode: 'HA1 4JD'}
-  )
-  stub_request_with_address("30 Pinner Road")
-  proposed_org.users << associated_user
-  proposed_org
-end
 Given(/^a proposed organisation has been proposed by "(.*)"$/) do |user_email|
   usr = User.find_by(email: user_email)
   unsaved_proposed_organisation(usr).save!
--- features/step_definitions/proposed_organisation_testing_api.rb
+++ features/step_definitions/proposed_organisation_testing_api.rb
@@ -2,10 +2,10 @@ require_relative 'map_testing_api.rb'
 
 def unsaved_proposed_organisation(associated_user = nil)
   proposed_org = ProposedOrganisation.new({name: "Friendly Chariity", description: "We are friendly!", 
-    email: "sample@sample.org", address: "30 Pinner Road", donation_info: "https://www.donate.com",
+    email: "sample@sample.org", address: "30 pinner road", donation_info: "https://www.donate.com",
     postcode: 'HA1 4JD'}
   )
-  stub_request_with_address("30 Pinner Road")
+  stub_request_with_address("30 pinner road")
   proposed_org.users << associated_user
   proposed_org
 end
