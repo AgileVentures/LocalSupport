5153d80540daa128d6a9f01d182e23efd60769f8
mtc2013 <mtc2013opensource@gmail.com>
2015-05-18 22:05:08 -0400

    Fix step

--- features/step_definitions/moderate_proposed_organisation_steps.rb
+++ features/step_definitions/moderate_proposed_organisation_steps.rb
@@ -1,12 +1,14 @@
 Then (/^I should( not)? see an "Accept Proposed Organisation" button$/) do |negation|
   if negation
     expect(page).not_to have_link "Accept"
+  else
+      expect(page).to have_link "Accept"
   end
-  expect(page).to have_link "Accept"
 end
 Then (/^I should( not)? see a "Reject Proposed Organisation" button$/) do |negation|
   if negation
     expect(page).not_to have_link "Reject"
+  else
+    expect(page).to have_link "Reject"
   end
-  expect(page).to have_link "Reject"
 end
