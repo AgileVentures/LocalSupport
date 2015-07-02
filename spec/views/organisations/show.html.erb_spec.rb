7960030195ff840fea6ff33683a7144d274b702f
icicleta <icicleta@gmail.com>
2015-07-02 10:46:10 +0100

    show view test passing

--- spec/views/organisations/show.html.erb_spec.rb
+++ spec/views/organisations/show.html.erb_spec.rb
@@ -230,16 +230,18 @@ describe 'organisations/show.html.erb', :type => :view do
       expect(rendered).not_to have_content "Sports"
     end
 
-    # it 'renders categories when present by type' do
-    #   cats = mock_model(Category::ActiveRecord::AssociationRelation)
-    #   org = mock_model(Organisation, categories: cats)
-    #   allow(cats).to receive(:what_they_do).and_return([mock_model(Category, name: "Animal Welfare"), mock_model(Category, name: "Sports")])
-    #   assign(:organisation, org)
-    #   render
-    #   expect(rendered).to have_content "Categories:"
-    #   expect(rendered).to have_content "Animal Welfare"
-    #   expect(rendered).to have_content "Sports"
-    # end
+    it 'renders categories when present by type' do
+      cats = double('Category::ActiveRecord_AssociationRelation')
+      org = mock_model(Organisation, categories: cats)
+      allow(cats).to receive_message_chain(:what_they_do, :pluck).and_return(["Animal Welfare", "Sports"])
+      allow(cats).to receive_message_chain(:who_they_help, :pluck).and_return(["Sports","Sports"])
+      allow(cats).to receive_message_chain(:how_they_help, :pluck).and_return(["Animal Welfare""Sports"])
+      assign(:organisation, org)
+      render
+      expect(rendered).to have_content "Categories:"
+      expect(rendered).to have_content "Animal Welfare"
+      expect(rendered).to have_content "Sports"
+    end
   end
 end
 
