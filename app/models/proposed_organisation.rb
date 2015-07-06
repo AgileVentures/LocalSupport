bc8cedc1d268ecb040265b69fb4191578936d330
mtc2013 <mtc2013opensource@gmail.com>
2015-07-06 19:06:43 -0400

    Highlight checkbox labels in red when they are source of validation error

--- app/models/proposed_organisation.rb
+++ app/models/proposed_organisation.rb
@@ -1,15 +1,12 @@
 class ProposedOrganisation < BaseOrganisation
   has_many :users, :foreign_key => 'organisation_id'
-  class MembershipEligibility < ActiveModel::Validator
-    def validate(record)
-      record.errors[:nonprofit] << "You must be a nonprofit organisation to join Harrow Community Network" unless record.non_profit 
-      unless record.works_in_harrow || record.registered_in_harrow
-        record.errors[:works_or_registered] << "You must be registered or work in Harrow to join Harrow Community Network"
-      end
+  validates_each :non_profit, :works_in_harrow, :registered_in_harrow  do  |record, attr, value|
+    if attr == :non_profit
+      record.errors.add(attr,"You must be a nonprofit organisation to join Harrow Community Network") unless record.non_profit
+    else
+      record.errors.add(attr,"You must be registered or work in Harrow to join Harrow Community Network") unless record.works_in_harrow || record.registered_in_harrow
     end
   end
-
-  validates_with MembershipEligibility
 
   def accept_proposal
     org = becomes!(Organisation)
