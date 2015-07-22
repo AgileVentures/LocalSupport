4802a4d74bcf813e60a23edc69f43b48b30ef855
icicleta <icicleta@gmail.com>
2015-07-22 10:32:37 +0100

    removed "sentence" from devise_helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -9,13 +9,9 @@ module DeviseHelper
     return "" if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
-      <!-- <h4>#{sentence}</h4> -->
       <ul>#{messages}</ul>
     </div>
     HTML
