3da4b97bb1545dfb34c40ded87b3feab21d3639e
icicleta <icicleta@gmail.com>
2015-07-28 11:18:54 +0100

    email regex refactored, now it doesn't pass if doesn't end in '.' and more letters

--- app/assets/javascripts/org_validation.js
+++ app/assets/javascripts/org_validation.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
 
   jQuery.validator.addMethod('emailVal', function(value, element) {
     // allow any non-whitespace characters as the host part
-    return this.optional( element ) || /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@(?:\S{1,63})$/.test( value );
+    mailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+    return this.optional( element ) || mailRegex.test( value );
   }, 'Please enter a valid email address');
 
 
