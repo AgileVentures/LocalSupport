797e2be2bfd489d5b51764bffa1c9ba0c51bae61
icicleta <icicleta@gmail.com>
2015-07-28 11:08:35 +0100

    styling error message and refactored org_validation

--- app/assets/javascripts/org_validation.js
+++ app/assets/javascripts/org_validation.js
@@ -1,28 +1,27 @@
-$(document).ready(function () {
 
-  jQuery.validator.setDefaults({
-    // where to display the error relative to the element
-    errorPlacement: function(error, element) {
-      error.insertBefore(element);
-    }
-  });
+$(document).ready(function () {
 
   jQuery.validator.addMethod('emailVal', function(value, element) {
     // allow any non-whitespace characters as the host part
     return this.optional( element ) || /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@(?:\S{1,63})$/.test( value );
-  }, 'Please enter a valid email address.');
+  }, 'Please enter a valid email address');
 
 
   $('.edit_organisation').validate({ // initialize the Plugin
 
     rules: {
 
-      "organisation[email]":
+      "organisation[email]": {
+          // other rules,
           required: true,
           emailVal: true // <-  declare the rule someplace!
       }
 
     },
+
+    errorPlacement: function(error, element) {
+      error.insertBefore(element);
+    }
 
   });
 
