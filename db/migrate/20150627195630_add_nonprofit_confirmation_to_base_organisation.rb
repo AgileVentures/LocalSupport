e4963cfbb06b88c2c8988e957152bd5d3ca19535
mtc2013 <mtc2013opensource@gmail.com>
2015-08-01 14:01:43 -0400

    Consolidate two migrations into one

--- db/migrate/20150627195630_add_nonprofit_confirmation_to_base_organisation.rb
+++ db/migrate/20150627195630_add_nonprofit_confirmation_to_base_organisation.rb
@@ -1,7 +1,5 @@
 class AddNonprofitConfirmationToBaseOrganisation < ActiveRecord::Migration
   def change
     add_column :organisations, :non_profit, :boolean
-    add_column :organisations, :registered_in_harrow, :boolean
-    add_column :organisations, :works_in_harrow, :boolean
   end
 end
--- db/migrate/20150717234054_remove_registered_working_fields_for_confirmation.rb
+++
@@ -1,6 +0,0 @@
-class RemoveRegisteredWorkingFieldsForConfirmation < ActiveRecord::Migration
-  def change
-    remove_column :organisations, :registered_in_harrow, :boolean
-    remove_column :organisations, :works_in_harrow, :boolean
-  end
-end
