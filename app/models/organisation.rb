59e0afc20d61be0b491c0fadf5cf878260a96685
mtc2013 <mtc2013opensource@gmail.com>
2015-08-27 11:22:30 -0400

    Change to guard use of if Make callback method private

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -34,10 +34,8 @@ class Organisation < BaseOrganisation
   #TODO: Give this TLC and refactor the flow or refactor out responsibilities
   # This method both adds new editors and/or updates attributes
   def update_attributes_with_superadmin(params)
-    email = extract_email_param params
-    if email.blank?
-      return self.update_attributes(params)   # explicitly call with return to return boolean instead of nil
-    end
+    email = extract_email_param(params)
+    return self.update_attributes(params) if email.blank?   # explicitly call with return to return boolean instead of nil
     #Transactions are protective blocks where SQL statements are only permanent if they can all succeed as one atomic action.
     ActiveRecord::Base.transaction do
       add_existing_user_or_create_anew email
@@ -151,13 +149,13 @@ class Organisation < BaseOrganisation
   def not_updated_recently_or_has_no_owner?
     self.users.empty? || not_updated_recently?
   end
-  
+
+  private
+
   def error_when_new_org_admin_invited email
     self.errors.add(:superadministrator_email, "The user email you entered,'#{email}', is invalid")
     raise ActiveRecord::Rollback    # is this necessary? Doesn't the transaction block rollback the change with `usr` if update_attributes fails?
   end
-  
-  private
 
   def add_existing_user_or_create_anew email
     usr = User.find_by_email(email)
