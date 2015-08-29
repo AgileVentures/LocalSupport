03fbe0792ffb3ad2dfa1a53c591752e9658a4114
Zmago Devetak <zmago_devetak@yahoo.com>
2015-08-29 16:08:13 +0000

    change vcr setup

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -4,10 +4,8 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.cassette_library_dir  = 'features/cassettes'
   c.debug_logger = File.open(ARGV.first, 'w')
-  c.allow_http_connections_when_no_cassette = true
 end
 
 VCR.cucumber_tags do |t|
-  t.tag '@disallowed', :record => :none
   t.tag  '@vcr', :use_scenario_name => true
 end
