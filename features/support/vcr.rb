9d175a8adc987a4d5437668a616da1f011c7111a
ZmagoD <zmago_devetak@yahoo.com>
2015-09-06 18:44:18 +0200

    set vcr config file to enable record new episodes

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -2,6 +2,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = true
+  c.default_cassette_options = { :record => :new_episodes }
   c.hook_into :webmock
   c.cassette_library_dir  = 'features/cassettes'
   #c.debug_logger = File.open(ARGV.first, 'w')
