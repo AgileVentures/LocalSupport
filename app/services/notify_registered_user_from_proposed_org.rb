f41965f84a691bdaf464f03b8bf0e39016f8d5fd
mtc2013 <mtc2013opensource@gmail.com>
2015-10-04 17:22:12 -0400

    Excise no longer used response object

--- app/services/notify_registered_user_from_proposed_org.rb
+++ app/services/notify_registered_user_from_proposed_org.rb
@@ -1,9 +1,4 @@
 class NotifyRegisteredUserFromProposedOrg
-  class Response
-    def success?
-      true
-    end
-  end
   def initialize(user,org)
     @user = user
     @org = org
@@ -12,6 +7,5 @@ class NotifyRegisteredUserFromProposedOrg
     @user.organisation = @org
     @user.save!
     OrgAdminMailer.notify_proposed_org_accepted(@org,@user.email).deliver_now
-    Response.new
   end
 end
