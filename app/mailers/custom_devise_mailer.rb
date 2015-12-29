1fb37b54a526458d1ec3101a4a9a876bb3cca3a0
Zhivko Siderov <zhivko.siderov@gmail.com>
2015-12-29 19:39:03 +0000

    Mailers - refactored

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -4,6 +4,12 @@ class CustomDeviseMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 
+  # The default values for any email we send from CustomDeviseMailer.
+  # Any value can be overridden on a per-email basis.
+  default from: "support@harrowcn.org.uk",
+          cc: "technical@harrowcn.org.uk",
+          reply_to: "support@harrowcn.org.uk"
+
   # from devise_invitable-1.2.1/lib/devise_invitable/mailer.rb
   def invitation_instructions(record, token, opts={})
     opts[:cc] = 'technical@harrowcn.org.uk'
@@ -15,10 +21,7 @@ class CustomDeviseMailer < Devise::Mailer
     @org = org
     @resource = usr
     mail(subject: "Your organisation has been approved for inclusion in the Harrow Community Network!",
-         to: [email],
-         from: "support@harrowcn.org.uk",
-         cc: "technical@harrowcn.org.uk",
-         reply_to: "support@harrowcn.org.uk")
+         to: [email])
   end
 
 end
