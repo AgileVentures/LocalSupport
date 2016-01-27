af8ae1962b71bc2980305d1684cacb61f8d05d06
samover <samuel.overloop@gmail.com>
2016-01-27 17:31:53 +0100

    implement email validation in model

--- app/models/base_organisation.rb
+++ app/models/base_organisation.rb
@@ -1,12 +1,15 @@
 class BaseOrganisation < ActiveRecord::Base
+  include ActiveModel::Validations
+
   acts_as_paranoid
   validates_url :website, :prefferred_scheme => 'http://', :if => Proc.new{|org| org.website.present?}
   validates_url :donation_info, :prefferred_scheme => 'http://', :if => Proc.new{|org| org.donation_info.present?}
   validates :name, presence: true
+  validates :email, email: true
   has_many :category_organisations, :foreign_key => :organisation_id
   has_many :categories, :through => :category_organisations, :foreign_key => :organisation_id
   accepts_nested_attributes_for :category_organisations,
-                                :allow_destroy => true
+    :allow_destroy => true
   # For the geocoder gem
   geocoded_by :full_address
   after_validation :geocode, if: -> { run_geocode? }
@@ -22,18 +25,18 @@ class BaseOrganisation < ActiveRecord::Base
   end
 
   def full_address
-     "#{self.address}, #{self.postcode}"
+    "#{self.address}, #{self.postcode}"
   end
 
   def gmaps4rails_marker_attrs
     if recently_updated_and_has_owner
       ['https://mt.googleapis.com/vt/icon/name=icons/spotlight/spotlight-poi.png',
-        'data-id' => id,
+       'data-id' => id,
        class: 'marker']
     else
       ['https://maps.gstatic.com/intl/en_ALL/mapfiles/markers2/measle.png',
-        'data-id' => id,
-        class: 'measle']
+       'data-id' => id,
+       class: 'measle']
     end
   end
 
