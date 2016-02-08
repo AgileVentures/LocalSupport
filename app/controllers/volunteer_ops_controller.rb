c5d30e63d0b68fd510f72c52fd527c6a0021d745
Proskurina <yproskurina@yahoo.com>
2016-02-08 17:30:11 +0000

    Moved request to doit to services get_from_doit.rb

--- app/controllers/volunteer_ops_controller.rb
+++ app/controllers/volunteer_ops_controller.rb
@@ -61,7 +61,7 @@ class VolunteerOpsController < ApplicationController
     harrow_markers = build_map_markers(organisations)
     doit_markers = '[]'
     if Feature.active? :doit_volunteer_opportunities
-      doit_orgs = collect_all_items
+      doit_orgs = GetFromDoit.run
       doit_markers = build_map_markers(doit_orgs, :doit, false)
     end
     markers = list_of_markers(harrow_markers, doit_markers)
@@ -91,34 +91,6 @@ class VolunteerOpsController < ApplicationController
         type: 'vol_op'
       )
     end
-  end
-
-  # This needs to be a helper method for the do-it API
-  def collect_all_items
-    host = 'https://api.do-it.org'
-    href = "/v1/opportunities\?lat\=51.5978\&lng\=-0.3370\&miles\=0.5 "
-    doit_orgs = []
-    id = 1
-    while href
-      url = host + href
-      response = HTTParty.get(url)
-      if response.body && response.body != '[]'
-        resp_items = JSON.parse(response.body)['data']['items']
-        resp_items.each do |item|
-          org = OpenStruct.new(latitude: item['lat'], longitude: item['lng'], 
-                opp_name: item['title'], id: id, 
-                description: item['description'], 
-                opportunity_id: item['id'], 
-                name: item['for_recruiter']['name'], 
-                org_link: item['for_recruiter']['slug'])
-          doit_orgs.push (org)
-          id += 1
-        end
-        next_page = JSON.parse(response.body)['links']['next']
-      end
-      href = next_page ? next_page['href'] : nil
-    end
-    doit_orgs
   end
 
   def authorize
