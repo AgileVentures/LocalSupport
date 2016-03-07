1ca6d80b7819f0a4b810a83ca82f599c2577f950
Sam Joseph <tansaku@gmail.com>
2016-03-07 16:42:38 +0000

    fleshes out the build markers with info window spec

--- app/services/build_markers_with_info_window.rb
+++ app/services/build_markers_with_info_window.rb
@@ -1,18 +1,18 @@
 class BuildMarkersWithInfoWindow
 
-  def self.with(volops, listener, marker_builder = Gmaps4rails)
-    new(volops,listener, marker_builder).send(:run)
+  def self.with(volops, listener, marker_builder = Gmaps4rails, helper = ActionController::Base.helpers)
+    new(volops, listener, marker_builder, helper).send(:run)
   end
-
 
   private
 
-  attr_reader :volops, :listener, :marker_builder
+  attr_reader :volops, :listener, :marker_builder, :helper
 
-  def initialize(volops,listener, marker_builder)
+  def initialize(volops, listener, marker_builder, helper)
     @volops = volops
     @listener = listener
     @marker_builder = marker_builder
+    @helper = helper
   end
 
   def run
@@ -20,20 +20,20 @@ class BuildMarkersWithInfoWindow
     marker_builder.build_markers(volops, &method(:build_single_marker)).to_json
   end
 
-  def build_single_marker(volop,marker)
+  def build_single_marker(volop, marker)
     marker.lat volop.latitude.nil? ? volop.organisation.latitude : volop.latitude
     marker.lng volop.longitude.nil? ? volop.organisation.longitude : volop.longitude
-    marker.infowindow listener.render_to_string(partial: "popup_#{volop.source}", locals: { volop: volop })
+    marker.infowindow listener.render_to_string(partial: "popup_#{volop.source}", locals: {volop: volop})
     marker.json(
-        custom_marker: listener.render_to_string(
-            partial: 'shared/custom_marker',
-            locals: { attrs: [ActionController::Base.helpers.asset_path("volunteer_icon_#{volop.source}.png"),
-                              'data-id' => volop.id,
-                              class: 'vol_op',
-                              title: "Click here to see volunteer opportunities at #{volop.title}"]}
-        ),
-        index: 1,
-        type: 'vol_op'
+      custom_marker: listener.render_to_string(
+        partial: 'shared/custom_marker',
+        locals: {attrs: [helper.asset_path("volunteer_icon_#{volop.source}.png"),
+                         'data-id' => volop.id,
+                         class: 'vol_op',
+                         title: "Click here to see volunteer opportunities at #{volop.title}"]}
+      ),
+      index: 1,
+      type: 'vol_op'
     )
   end
 
