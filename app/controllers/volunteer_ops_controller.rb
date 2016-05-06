03c06fbdccc44f7392cf2e5c80ef4b9db6261817
Zmago Devetak <zmago_devetak@yahoo.com>
2016-05-06 10:44:54 +0000

    adds volunteer ops address in volunteer_ops show template if is set

--- app/controllers/volunteer_ops_controller.rb
+++ app/controllers/volunteer_ops_controller.rb
@@ -72,6 +72,7 @@ class VolunteerOpsController < ApplicationController
       :description,
       :title,
       :organisation_id,
+      :volunteer_op_loc
     )
   end
 
--- app/helpers/volunteer_ops_helper.rb
+++ app/helpers/volunteer_ops_helper.rb
@@ -2,4 +2,8 @@ module VolunteerOpsHelper
   def button_text object
     object.new_record? ? 'Create a Volunteer Opportunity' : 'Update a Volunteer Opportunity'
   end
+  
+  def has_diff_volunteer_op_location? object
+    !object.volunteer_op_loc.empty?
+  end
 end
