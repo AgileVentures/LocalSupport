60ec765b20e10272d08cd3f13ff0ff05cb6b2171
Brychu <bryszard@poczta.onet.pl>
2016-05-18 10:07:03 +0000

    Refactored slug parser's methods in string_spec

--- spec/libs/string_spec.rb
+++ spec/libs/string_spec.rb
@@ -2,26 +2,22 @@ require 'rails_helper'
 require 'string.rb'
 
 describe String do
-  let(:org) { described_class.new }
-  
+
   describe '#short_name' do
     
     it 'as default returns first three words joined by dashes' do
-      org.concat 'better world charity london'
-      slug = org.short_name
-      expect(slug).to eq('better-world-charity')
+      org = 'better world charity london'
+      expect(org.short_name).to eq('better-world-charity')
     end
     
     it 'creating slug omits words - the, of, for, and, in, to' do
-      org.concat 'to the and in of better world for charity'
-      slug = org.short_name
-      expect(slug).to eq('better-world-charity')
+      org = 'to the and in of better world for charity'
+      expect(org.short_name).to eq('better-world-charity')
     end
 
     it 'returns special parochial church slug when organisation is a parochial church' do
-      org.concat 'parochial church parish of st lucas'
-      slug = org.short_name
-      expect(slug).to eq('parochial-church-st-lucas')
+      org = 'parochial church parish of st lucas'
+      expect(org.short_name).to eq('parochial-church-st-lucas')
     end
   
   end
@@ -29,15 +25,13 @@ describe String do
   describe '#longer_name' do
     
     it 'returns first three words and two last words joined by dashes' do
-      org.concat 'better world charity london robert baratheon'
-      slug = org.longer_name
-      expect(slug).to eq('better-world-charity-robert-baratheon')
+      org = 'better world charity london robert baratheon'
+      expect(org.longer_name).to eq('better-world-charity-robert-baratheon')
     end
     
     it 'creating slug omits words - the, of, for, and, in, to' do
-      org.concat 'to the and in of better world for charity london robert baratheon'
-      slug = org.longer_name
-      expect(slug).to eq('better-world-charity-robert-baratheon')
+      org = 'to the and in of better world for charity london robert baratheon'
+      expect(org.longer_name).to eq('better-world-charity-robert-baratheon')
     end
 
   end
@@ -45,15 +39,13 @@ describe String do
   describe '#append_org' do
 
     it 'returns all words and word "org" joined by dashes' do
-      org.concat 'better world charity london robert baratheon'
-      slug = org.append_org
-      expect(slug).to eq('better-world-charity-london-robert-baratheon-org')
+      org = 'better world charity london robert baratheon'
+      expect(org.append_org).to eq('better-world-charity-london-robert-baratheon-org')
     end
     
     it 'creating slug omits words - the, of, for, and, in, to' do
-      org.concat 'to the and in of better world for charity london robert baratheon'
-      slug = org.append_org
-      expect(slug).to eq('better-world-charity-london-robert-baratheon-org')
+      org = 'to the and in of better world for charity london robert baratheon'
+      expect(org.append_org).to eq('better-world-charity-london-robert-baratheon-org')
     end
 
   end
