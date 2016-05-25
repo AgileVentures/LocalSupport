aa7cb0d498e8aa959f321d5b3d8dcbb00bafc7d8
Brychu <bryszard@poczta.onet.pl>
2016-05-25 16:49:33 +0000

    Admin can now edit invitation mails (#78643156)

---
+++ app/controllers/mail_templates_controller.rb
@@ -0,0 +1,26 @@
+class MailTemplatesController < ApplicationController
+  before_action :authorize
+  
+  def edit
+    @mail_template = current_user.mail_template
+    @orphans = Organisation.not_null_email.null_users.without_matching_user_emails
+    render :template => 'organisation_reports/without_users_edit', :layout => 'invitation_table'
+  end
+  
+  def update
+    @mail_template = MailTemplate.find(params[:id])
+    @orphans = Organisation.not_null_email.null_users.without_matching_user_emails
+    if @mail_template.update_attributes(mail_params)
+      flash[:notice] = "Mail template has been updated"
+      redirect_to organisations_report_path
+    else
+      redirect_to :back
+    end
+  end
+  
+  private
+  
+  def mail_params
+    params.require(:mail_template).permit(:text, :footnote)
+  end
+end
