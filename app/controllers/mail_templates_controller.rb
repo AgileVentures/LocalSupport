7bc2872e83ad460b523c6052ea56f24bd195544e
Pedro Brychowski <pbrych@gmail.com>
2016-06-06 22:23:31 +0200

    Refactor over PR comments, v.4

--- app/controllers/mail_templates_controller.rb
+++ app/controllers/mail_templates_controller.rb
@@ -11,12 +11,9 @@ class MailTemplatesController < ApplicationController
   def update
     @mail_template = MailTemplate.find(params[:id])
     @orphans = Organisation.not_null_email.null_users.without_matching_user_emails
-    if @mail_template.update_attributes(mail_params)
-      flash[:notice] = 'Mail template has been updated'
-      redirect_to organisations_report_path
-    else
-      redirect_to :back
-    end
+    (redirect_to :back and return) unless @mail_template.update_attributes(mail_params)
+    flash[:notice] = 'Mail template has been updated'
+    redirect_to organisations_report_path
   end
   
   private
