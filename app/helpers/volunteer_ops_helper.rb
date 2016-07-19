89562f041a16a36e8984906c138451a2abd8ea04
Zmago Devetak <zmago_devetak@yahoo.com>
2016-07-19 20:20:12 +0000

    refactor PR #329

--- app/helpers/volunteer_ops_helper.rb
+++ app/helpers/volunteer_ops_helper.rb
@@ -2,4 +2,9 @@ module VolunteerOpsHelper
   def button_text object
     object.new_record? ? 'Create a Volunteer Opportunity' : 'Update a Volunteer Opportunity'
   end
+
+  def link_to_vol_op(obj, name, link, html_options = {})
+    html_options.merge!(target: "_blank") unless obj.source == 'local'
+    link_to(name, link, html_options)
+  end
 end
