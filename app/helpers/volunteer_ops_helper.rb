af64decc11820883864ee9e6586107a2e13f3637
Zmago Devetak <zmago_devetak@yahoo.com>
2016-07-24 18:39:02 +0000

    refactor link_to_vol_op

--- app/helpers/volunteer_ops_helper.rb
+++ app/helpers/volunteer_ops_helper.rb
@@ -3,8 +3,9 @@ module VolunteerOpsHelper
     object.new_record? ? 'Create a Volunteer Opportunity' : 'Update a Volunteer Opportunity'
   end
 
-  def link_to_vol_op(obj, name, link, html_options = {})
+  def link_to_vol_op(obj, name, html_options = {})
     html_options.merge!(target: "_blank") unless obj.source == 'local'
-    link_to(name, link, html_options)
+    return link_to( obj.title, obj.link, html_options) if name == 'title'
+    link_to(obj.organisation_name, obj.organisation_link, html_options)
   end
 end
