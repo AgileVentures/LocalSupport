bec16579109b87e858c328e261d5eb7fe8c4bb14
Sam Joseph <tansaku@gmail.com>
2016-08-31 10:21:56 +0100

    tweaks the method signature to indicate there are different types of links

--- app/helpers/volunteer_ops_helper.rb
+++ app/helpers/volunteer_ops_helper.rb
@@ -3,9 +3,9 @@ module VolunteerOpsHelper
     object.new_record? ? 'Create a Volunteer Opportunity' : 'Update a Volunteer Opportunity'
   end
 
-  def link_to_vol_op(obj, name, html_options = {})
+  def link_to_vol_op(obj, type, html_options = {})
     html_options[:target] = '_blank' unless obj.source == 'local'
-    return link_to( obj.title, obj.link, html_options) if name == 'title'
+    return link_to(obj.title, obj.link, html_options) if type == :title
     link_to(obj.organisation_name, obj.organisation_link, html_options)
   end
 end
