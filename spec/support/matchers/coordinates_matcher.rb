d1d0c3afe923c9db7761d49f3b8e8fc86fc7940f
Sam Joseph <tansaku@gmail.com>
2016-09-23 13:37:50 +0100

    Update coordinates_matcher.rb

--- spec/support/matchers/coordinates_matcher.rb
+++ spec/support/matchers/coordinates_matcher.rb
@@ -1,4 +1,4 @@
-RSpec::Matchers.define :have_coordinates do |expected|
+RSpec::Matchers.define :have_coordinates do |_expected|
   match do |actual|
     expect(actual.longitude).not_to be nil
     expect(actual.latitude).not_to be nil
