9d534c582c8d8a6cbb82fbc2990dc285969b4069
Marouen Bousnina <marouen.it.technology@gmail.com>
2016-12-16 13:10:03 +0100

    Remove gmaps4rails gem [fixes #134262481] (#397)
    * remove gmaps4rails gem [fixes #134262481]
    
    Addresses: https://www.pivotaltracker.com/story/show/134262481
    
    * Add @maps tags and fix code climate warning

--- spec/javascripts/google_map/map_settings_spec.js
+++ spec/javascripts/google_map/map_settings_spec.js
@@ -1,23 +1,11 @@
-describe('LocalSupport.MapSettings', function() {
-  var google;
+describe('Maps settings', function() {
 
-  beforeEach(function() {
-    google = {
-      maps: {
-        LatLng: function(lat, lng) {
-          this.msg = 'I am just a mock object!'
-          this.lat = lat;
-          this.lng = lng;
-        }
-      }
-    }
-  });
+  it('expresses default settings', function() {
 
-  it('expresses default settings in terms of internal and provider objects', function() {
-    var settings = LocalSupport.MapSettings().for(google);
-    expect(settings.internal.id).toBe('map_canvas');
-    expect(settings.provider.zoom).toBe(12);
-    expect(settings.provider.center.lat).toBe(51.5978);
-    expect(settings.provider.center.lng).toBe(-0.337);
+    expect(Settings.id).toBe('map-canvas');
+    expect(Settings.zoom).toBe(12);
+    expect(Settings.lat).toBe(51.5978);
+    expect(Settings.lng).toBe(-0.337);
+
   });
 });
