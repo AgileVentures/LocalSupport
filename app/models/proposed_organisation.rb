e8aa731409f404da2c51592b5684cfbb7fea38d1
Olena_Ch <master_of_time@bigmir.net>
2016-12-28 00:33:52 +0000

    Refactoring flash messages into a service

--- app/models/proposed_organisation.rb
+++ app/models/proposed_organisation.rb
@@ -1,12 +1,8 @@
 class ProposedOrganisation < BaseOrganisation
   has_many :users, :foreign_key => 'organisation_id'
   
-  def accept_proposal(rollback_acception = false)
-    if rollback_acception
-      org = becomes!(ProposedOrganisation)
-    else
-      org = becomes!(Organisation)
-    end
+  def accept_proposal(rollback = false)
+    org = rollback ? becomes!(ProposedOrganisation) : becomes!(Organisation)
     org.save!
     org
   end
--- app/services/create_flash_for_proposed_organisation.rb
+++ app/services/create_flash_for_proposed_organisation.rb
@@ -1,61 +1,57 @@
 class CreateFlashForProposedOrganisation
-  class Accepted
-    def self.results
-      { invited: AcceptProposedOrganisation::Response::INVITATION_SENT,
-        notified: AcceptProposedOrganisation::Response::NOTIFICATION_SENT }
-    end
-  end
+  ACCEPTED = {
+    invited: AcceptProposedOrganisation::Response::INVITATION_SENT,
+    notified: AcceptProposedOrganisation::Response::NOTIFICATION_SENT
+  }.freeze
   
-  class NotAccepted
-    def self.results
-      { invalid_email: AcceptProposedOrganisation::Response::INVALID_EMAIL,
-        no_email: AcceptProposedOrganisation::Response::NO_EMAIL }
-    end
-  end
+  NOT_ACCEPTED = {
+    invalid_email: AcceptProposedOrganisation::Response::INVALID_EMAIL,
+    no_email: AcceptProposedOrganisation::Response::NO_EMAIL
+  }.freeze
   
   def initialize obj
     @obj = obj
-    @rslt = {}
+    @rsl = {}
   end
   
   def accepted?
-    return true if [ Accepted.results[:invited], 
-                     Accepted.results[:notified] ].include?(@obj.status)
+    return true if [ ACCEPTED[:invited], 
+                     ACCEPTED[:notified] ].include?(@obj.status)
     false
   end
   
   def flashes_for_accepted_org
-    @rslt[:notice] = ['You have approved the following organisation']
+    @rsl[:notice] = ['You have approved the following organisation']
     if_invitation_sent; if_notification_sent
-    @rslt
+    @rsl
   end
   
   def flash_for_not_accepted_org
-    @rslt[:error] = "No mail was sent because: #{@obj.error_message}"
+    @rsl[:error] = "No mail was sent because: #{@obj.error_message}"
     if_invalid_email; if_no_email
-    @rslt
+    @rsl
   end
   
   def if_invalid_email
-    @rslt[:error] = "No invitation email was sent because the email associated \
-    with #{@obj.not_accepted_org.name}, #{@obj.not_accepted_org.email}, \
-    seems invalid" if @obj.status == NotAccepted.results[:invalid_email]
+    @rsl[:error] = "No invitation email was sent because the email associated" +
+    " with #{@obj.not_accepted_org.name}, #{@obj.not_accepted_org.email}," +
+    " seems invalid" if @obj.status == NOT_ACCEPTED[:invalid_email]
   end
   
   def if_invitation_sent
-    @rslt[:notice] << "An invitation email was sent to \
-    #{@obj.accepted_org.email}" if @obj.status == Accepted.results[:invited]
+    @rsl[:notice] << "An invitation email was sent to" +
+    " #{@obj.accepted_org.email}" if @obj.status == ACCEPTED[:invited]
   end
   
   def if_no_email
-    @rslt[:error] = "No invitation email was sent \
-    because no email is associated with the \
-    organisation" if @obj.status == NotAccepted.results[:no_email]
+    @rsl[:error] = "No invitation email was sent" +
+    " because no email is associated with the " +
+    "organisation" if @obj.status == NOT_ACCEPTED[:no_email]
   end
   
   def if_notification_sent
-    @rslt[:notice] << "A notification of acceptance was sent to \
-    #{@obj.accepted_org.email}" if @obj.status == Accepted.results[:notified]
+    @rsl[:notice] << "A notification of acceptance was sent to " +
+    "#{@obj.accepted_org.email}" if @obj.status == ACCEPTED[:notified]
   end
   
   def run
