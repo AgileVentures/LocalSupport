e7e8415745efe0711fa6986f277647cf9fd35dac
ZmagoD <zmago_devetak@yahoo.com>
2017-01-12 22:05:17 +0100

    fix migration down method

--- db/migrate/20170110213946_add_reach_skills_data_to_volunteer_ops.rb
+++ db/migrate/20170110213946_add_reach_skills_data_to_volunteer_ops.rb
@@ -7,6 +7,9 @@ class AddReachSkillsDataToVolunteerOps < ActiveRecord::Migration
   end
 
   def down
+    remove_column :volunteer_ops, :reachskills_org_name
+    remove_column :volunteer_ops, :reachskills_org_link
+    
     Feature.find_by(name: :reachskills_volunteer_opportunities).destroy
   end
 end
