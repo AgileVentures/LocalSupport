5428208dbfd139adf00127474191b24ac4801bf3
Sam Joseph <tansaku@gmail.com>
2017-01-12 16:05:50 +0000

    removes comments and other fix for codeclimate

--- app/controllers/volunteer_ops_controller.rb
+++ app/controllers/volunteer_ops_controller.rb
@@ -19,7 +19,7 @@ class VolunteerOpsController < ApplicationController
   end
 
   def show
-    render template: 'pages/404', status: 404 and return if @volunteer_op.nil?
+    render template: 'pages/404', status: 404 && return if @volunteer_op.nil?
     @organisation = Organisation.friendly.find(@volunteer_op.organisation_id)
     organisations = Organisation.where(id: @organisation.id)
     @editable = current_user.can_edit?(@organisation) if current_user
@@ -76,10 +76,6 @@ class VolunteerOpsController < ApplicationController
   end
 
   def authorize
-    # set @organisation
-    # then can make condition:
-    # unless current_user.can_edit? organisation
-
     unless org_owner?
       flash[:error] = 'You must be signed in as an organisation owner or ' \
                       'site superadmin to perform this action!'
