903b69bf199ed87701d08f0047e5653617b92442
Olena_Ch <master_of_time@bigmir.net>
2017-04-24 23:32:00 +0000

    [50085449] Google map popup logic

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,6 +3,7 @@
 
 var map;
 var markers = [];
+var items = [];
 var Settings = {
   id: 'map-canvas',
   lat: 51.5978,
@@ -19,25 +20,20 @@ function initMap() {
   var markerData = $("#marker_data").data().markers;
   var marker;
 
-  var ib = new InfoBox();
-  var ibOptions = { pixelOffset: new google.maps.Size(-151, 10) };
-  var boxText = document.createElement("div");
-  boxText.setAttribute("class", "arrow_box")
-  ibOptions.content = boxText;
+  var ib = createInfoBox();
 
   markerData.forEach(function(item) {  
     var latLng = new google.maps.LatLng(item.lat, item.lng);
     marker = new CustomMarker(latLng, map, { content: item.custom_marker });
     
     google.maps.event.addListener(marker, 'click', function() {
-        ib.setOptions(ibOptions);
-        boxText.innerHTML = item.infowindow;
-        $(ib.content_).find('.close').click(function(){ ib.close(); });
-        ib.open(map, this);
-        map.panTo(ib.getPosition());
+      setInfoBoxInnerContent(ib, item.infowindow);
+      ib['infoBox'].open(map, this);
+      map.panTo(ib['infoBox'].getPosition());
     });
     
     markers.push(marker);
+    items.push(item);
   });
 }
 
@@ -46,13 +42,37 @@ function centerMap(lat, lng) {
   map.setZoom(15);
 }
 
+function createInfoBox() {
+  var ib = new InfoBox(),
+  ibOptions = { pixelOffset: new google.maps.Size(-151, 10) }, 
+  boxText = document.createElement("div");
+  
+  boxText.setAttribute("class", "arrow_box")
+  ibOptions.content = boxText;
+  ib.setOptions(ibOptions);
+  
+  return { infoBox: ib, infoBoxOptions: ibOptions, infoBoxText: boxText };
+}
+
 function openInfoBox(lat, lng) {
-  var contentString = '<div id="content"><p>Charity</p></div>';
-  var ib = new google.maps.InfoWindow({ content: contentString });
+  var ib = createInfoBox();
+  
   markers.forEach(function(item) {
     var posn = item.getPosition();
-    if (posn.lat() == lat && posn.lng().toFixed(6) == lng.toFixed(6))
-      ib.open(map, item);
+    if (posn.lat() == lat && posn.lng().toFixed(6) == lng.toFixed(6)) {
+      items.forEach(function(i) {
+        if (i.lat == lat && i.lng == lng)
+          setInfoBoxInnerContent(ib, i.infowindow);
+      })
+      ib['infoBox'].open(map, item);
+    }
+  });
+}
+
+function setInfoBoxInnerContent(ib, info) {
+  ib['infoBoxText'].innerHTML = info;
+  $(ib['infoBox'].content_).find('.close').click(function(){ 
+    ib['infoBox'].close();
   });
 }
 
@@ -64,8 +84,9 @@ $(document).ready(function() {
   }
   
   $('.organisation-title').click(function () {
-    var lat = parseFloat($(this).attr('data-lat'));
-    var lng = parseFloat($(this).attr('data-lng'));
+    var lat = parseFloat($(this).attr('data-lat')),
+    lng = parseFloat($(this).attr('data-lng'));
+    
     centerMap(lat, lng);
     openInfoBox(lat, lng);
   });
