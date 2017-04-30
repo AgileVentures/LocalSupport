5126ce13e39a8c43c44ce23b3e47b86abf130ad2
Olena_Ch <master_of_time@bigmir.net>
2017-04-30 00:44:18 +0000

    Increasing RSpec coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,46 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-  describe ".permit" do
-    it "returns the cleaned params" do
-      user_params = {id: 5, email: "a@a.com", password: "blahblah", password_confirmation: "blahblah", 
-       remember_me: true, pending_organisation_id: 5, superadmin: true}
+  let(:user) { FactoryGirl.create(:user) }
+  
+  describe '.permit' do
+    it 'returns the cleaned params' do
+      user_params = {
+        id: 5, 
+        email: "a@a.com", 
+        password: "blahblah", 
+        password_confirmation: "blahblah", 
+        remember_me: true, 
+        pending_organisation_id: 5, 
+        superadmin: true
+      }
       params = ActionController::Parameters.new.merge(user_params)
       permitted_params = UsersController::UserParams.build(params)
-      expect(permitted_params).to eq({id: 5, email: "a@a.com", password: "blahblah", password_confirmation: "blahblah", 
-       remember_me: true, pending_organisation_id: 5}.with_indifferent_access)
+      
+      expect(permitted_params).to eq({
+                                      id: 5, 
+                                      email: "a@a.com", 
+                                      password: "blahblah", 
+                                      password_confirmation: "blahblah",
+                                      remember_me: true, 
+                                      pending_organisation_id: 5
+                                    }.with_indifferent_access)
+    end
+  end
+  
+  describe 'edit' do
+    before(:each) { get :edit, id: user.id }
+    
+    it 'assigns @volunteer_ops variable' do
+      expect(assigns(:user)).to be_instance_of User
+    end
+    
+    it 'returns status 200' do
+      expect(response.status).to eq 200
+    end
+    
+    it 'renders index template' do
+      expect(response).to render_template(:edit)
     end
   end
 end
