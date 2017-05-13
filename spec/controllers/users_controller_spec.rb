7d3db39ae869b0a677daa518968dc02860bcdf5b
Olena_Ch <master_of_time@bigmir.net>
2017-05-13 23:33:34 +0000

    50085449 remove users controller rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-  let(:user) { FactoryGirl.create(:user) }
-  
   describe '.permit' do
     it 'returns the cleaned params' do
       user_params = {
@@ -25,22 +23,6 @@ describe UsersController, :type => :controller do
                                       remember_me: true, 
                                       pending_organisation_id: 5
                                     }.with_indifferent_access)
-    end
-  end
-  
-  describe 'edit' do
-    before(:each) { get :edit, id: user.id }
-    
-    it 'assigns @user variable' do
-      expect(assigns(:user)).to be_instance_of User
-    end
-    
-    it 'returns status 200' do
-      expect(response.status).to eq 200
-    end
-    
-    it 'renders index template' do
-      expect(response).to render_template(:edit)
     end
   end
 end
