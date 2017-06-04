db3679197ddbb3ca65ec6e8584e7206e3b1e26aa
lokeshh <lokeshhsharma@gmail.com>
2017-06-04 06:33:33 +0000

    replace validates_presence_of syntax [Finishes #146610007]

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ActiveRecord::Base
   #attr_accessible :content, :name, :permalink, :link_visible
-  validates_presence_of :name, :permalink
+  validates :name, presence: true
+  validates :permalink, presence: true
   validates_uniqueness_of :permalink
 
   # Provides links for page footers
