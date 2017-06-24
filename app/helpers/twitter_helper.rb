823a3d43be1af3dee41abc46fb24add6a827fcb8
Frank DelPidio <frankdaedalus@gmail.com>
2017-06-24 13:22:08 -0400

    Update name from client to twitter_client

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,10 +1,9 @@
 module TwitterHelper
 
 
-
   private
 
-  def client
+  def twitter_client
     Twitter::REST::Client.new do |config|
       config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
--- spec/helpers/twitter_helper_spec.rb
+++ spec/helpers/twitter_helper_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 describe TwitterHelper, :type => :helper do
   describe 'Configure Client' do
     it 'should have a consumer key' do
-      expect(client.consumer_key).to eq(ENV["TWITTER_CONSUMER_KEY"])
+      expect(twitter_client.consumer_key).to eq(ENV["TWITTER_CONSUMER_KEY"])
     end
     it 'should have a consumer secret' do
-      expect(client.consumer_secret).to eq(ENV["TWITTER_CONSUMER_SECRET"])
+      expect(twitter_client.consumer_secret).to eq(ENV["TWITTER_CONSUMER_SECRET"])
     end
     it 'should have an access token' do
-      expect(client.access_token).to eq(ENV["TWITTER_ACCESS_TOKEN"] )
+      expect(twitter_client.access_token).to eq(ENV["TWITTER_ACCESS_TOKEN"] )
     end
     it 'should have a secret access token' do
-      expect(client.access_token_secret).to eq(ENV["TWITTER_ACCESS_TOKEN_SECRET"])
+      expect(twitter_client.access_token_secret).to eq(ENV["TWITTER_ACCESS_TOKEN_SECRET"])
     end
-
   end
 end
