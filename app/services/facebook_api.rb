4851e252833cdd055de81c2e5388e214e1ada0db
Frank DelPidio <frankdaedalus@gmail.com>
2017-06-25 17:17:38 -0400

    Fix codeclimate issues

--- app/services/facebook_api.rb
+++ app/services/facebook_api.rb
@@ -2,10 +2,10 @@ class FacebookApi
   attr_reader :client
 
   def initialize
-    @client = Koala::Facebook::API.new()
+    @client = Koala::Facebook::API.new
   end
 
   def post_to_facebook(msg)
-    client.put_connections("me", "feed", message: msg)
+    client.put_connections('me', 'feed', message: msg)
   end
 end
