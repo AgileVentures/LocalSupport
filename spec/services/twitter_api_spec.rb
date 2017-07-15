0ce2e6fab8798d564e55dbbe5f54977cd84ec0cf
Frank DelPidio <frankdaedalus@gmail.com>
2017-07-14 20:24:16 -0400

    Rewrite twitter tests without creating extra methods

--- spec/services/twitter_api_spec.rb
+++ spec/services/twitter_api_spec.rb
@@ -6,25 +6,23 @@ describe TwitterApi do
     WebMock.allow_net_connect!
   end
 
-  after(:each) do
-    @twitter_client.delete_last_tweet
-  end
-
   after(:all) do
     WebMock.disable_net_connect!
   end
 
   context 'Posting to twitter' do
     it 'should be able to post 140 chars or less to twitter' do
-      tweet = "Test tweet"
+      number_of_tweets = @twitter_client.client.user.tweets_count
+      tweet = "Test Tweet #{number_of_tweets}"
       @twitter_client.post_to_twitter(tweet)
-      expect(@twitter_client.last_tweet).to eq(tweet)
+
+      expect(@twitter_client.client.user.tweets_count).to eq(number_of_tweets + 1)
     end
 
     it 'should be able to truncate longer messages to 140 chars and post to twitter' do
       tweet = "This string is going to be greater than 140 characters so we can test that our truncate method is functioning correctly. This is number 140. All of these characters should be truncated."
-      @twitter_client.post_to_twitter(tweet)
-      expect(@twitter_client.last_tweet).to eq(tweet[0..139])
+
+      expect(@twitter_client.tweet_size(tweet).length).to eq(140)
     end
 
   end
