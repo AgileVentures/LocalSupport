43f49c287335f9d84202f4cc532392e16dfcf8da
Marouen Bousnina <marouen.it.technology@gmail.com>
2017-08-20 10:30:35 +0200

    Handle doit creation duplication [Finishes #150247828]
    PT: https://www.pivotaltracker.com/story/show/150247828
    - Store id of created Doit volunteer op via LocalSupport services
    - Verify the existence of Doit volunteer op in trace history before
      importing

--- app/jobs/post_to_doit_job.rb
+++ app/jobs/post_to_doit_job.rb
@@ -1,11 +1,13 @@
 class PostToDoitJob < ActiveJob::Base
   queue_as :default
 
-  def perform(options, doit_post_handler = Doit::PostToDoit)
-    doit_post_handler.call(volunteer_op: options.fetch(:volunteer_op),
+  def perform(options, doit_post_handler = Doit::PostToDoit, trace_handler = DoitTrace)
+    volunteer_op = options.fetch(:volunteer_op)
+    doit_volop_id = doit_post_handler.call(volunteer_op: volunteer_op,
                            advertise_start_date: options.fetch(:advertise_start_date),
                            advertise_end_date: options.fetch(:advertise_end_date),
                            doit_org_id: options.fetch(:doit_org_id)
                           )
+    trace_handler.add_entry(volunteer_op.id, doit_volop_id)
   end
 end
