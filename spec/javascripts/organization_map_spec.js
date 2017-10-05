e23e7c81b7c4b61e239d13d307333235a99e9a3e
Sam Joseph <tansaku@gmail.com>
2017-10-05 15:32:18 +0100

    add callThrough to jasmine spy

--- spec/javascripts/organization_map_spec.js
+++ spec/javascripts/organization_map_spec.js
@@ -3,13 +3,14 @@ describe('Organization map', function() {
   beforeEach(function() {
 
   });
+
   describe('when hovering the pointer over an organization', function() {
     describe('and the organization have valid coordinates', function() {
       beforeEach(function() {
         loadFixtures('organization_list.html');
         OrganizationMap.initMap();
         OrganizationMap.init();
-        spyOn(OrganizationMap, 'getVolOpCoordinates');
+        spyOn(OrganizationMap, 'getVolOpCoordinates').and.callThrough();
         $('#valid-organization').trigger('mouseenter');
       });
       it('gets organization coordinates', function() {
