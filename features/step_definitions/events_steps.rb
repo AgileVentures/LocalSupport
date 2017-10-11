aada0d587ced42b45115f82c916d4ad2224e0a38
Brent Vardy <brent.vardy@challengelogic.net>
2017-10-11 20:10:13 +0100

    fix tests for new event datepicker

--- features/step_definitions/events_steps.rb
+++ features/step_definitions/events_steps.rb
@@ -2,17 +2,8 @@ Given (/^I fill in the new event page validly$/) do
   fill_in 'event_title', with: 'Hackathon'
   fill_in 'event_description', with: 'Great place to brain storm'
 
-  select '2018', from: 'event_start_date_1i'
-  select 'April', from: 'event_start_date_2i'
-  select '20', from: 'event_start_date_3i'
-  select '08', from: 'event_start_date_4i'
-  select '28', from: 'event_start_date_5i'
-
-  select '2018', from: 'event_end_date_1i'
-  select 'April', from: 'event_end_date_2i'
-  select '10', from: 'event_end_date_3i'
-  select '15', from: 'event_end_date_4i'
-  select '30', from: 'event_end_date_5i'
+  fill_in 'event_start_date', with: '20/04/2018'
+  fill_in 'event_end_date', with: '28/04/2018'
 end
 
 Given /^I create "(.*?)" event$/ do |name|
