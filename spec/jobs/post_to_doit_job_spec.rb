9ccd35b70f41f21797cee3cd05595ac006e83f3e
Marouen Bousnina <marouen.it.technology@gmail.com>
2017-10-18 19:00:27 +0200

    150247828 handle doit create dup (#470)
    * Update schema
    
    * Handle doit creation duplication [Finishes #150247828]
    
    PT: https://www.pivotaltracker.com/story/show/150247828
    - Store id of created Doit volunteer op via LocalSupport services
    - Verify the existence of Doit volunteer op in trace history before
      importing
    
    * fix code climate
    
    * Add cucumber test
    
    * Fix codeclimate issues

--- spec/jobs/post_to_doit_job_spec.rb
+++ spec/jobs/post_to_doit_job_spec.rb
@@ -4,15 +4,21 @@ include ActiveJob::TestHelper
 RSpec.describe PostToDoitJob, type: :job do
   describe '#perform' do
     it 'execute post to doit service' do
+      trace_double = double(add_entry: true)
       allow(Doit::PostToDoit).to receive(:call)
 
-      PostToDoitJob.new.perform(volunteer_op: nil,
+      PostToDoitJob.new.perform(
+                                {volunteer_op: double(id: 2),
                                  advertise_start_date: nil,
                                  advertise_end_date: nil,
                                  doit_org_id: nil
+                                },
+                                Doit::PostToDoit,
+                                trace_double
                                  )
 
       expect(Doit::PostToDoit).to have_received(:call)
+      expect(trace_double).to have_received(:add_entry)
     end
   end
 end
