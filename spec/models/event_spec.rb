b21ba2f6f6e68c34b0dba9716a88af54e90ee075
Ngalim Siregar <ngalim.siregar@gmail.com>
2017-10-31 20:04:28 +0900

    fix quote and indentation error & chg DateTime.now with DateTime.current

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -30,28 +30,28 @@ RSpec.describe Event, type: :model do
     expect(subject).to_not be_valid
   end
 
-  describe "scopes" do
+  describe 'scopes' do
 
     before(:all) do
       FactoryGirl.create_list(:upcoming_events, 10)
     end
-    
-    it "should have a valid upcoming method" do
+
+    it 'should have a valid upcoming method' do
       expect { Event.upcoming(10) }.not_to raise_error
     end
 
-    it "should return a variable number of upcoming events" do
+    it 'should return a variable number of upcoming events' do
       expect(Event.upcoming(10).length).to eq 10
     end
 
-   it "should not include events that are already over" do
-     FactoryGirl.create_list(:previous_events, 10)
-     expect(Event.upcoming(20).length).to eq 10
-   end
+    it 'should not include events that are already over' do
+      FactoryGirl.create_list(:previous_events, 10)
+      expect(Event.upcoming(20).length).to eq 10
+    end
 
-   it "should only contain events that are after the current datetime" do
-     expect(Event.upcoming(20)).to all (have_attributes(:start_date => (a_value > DateTime.now())))
-   end
+    it 'should only contain events that are after the current datetime' do
+      expect(Event.upcoming(20)).to all (have_attributes(start_date: (a_value > DateTime.current)))
+    end
 
   end
 
