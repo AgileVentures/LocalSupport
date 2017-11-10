13f86ea36c38c029b3abcd5ba049f0d4355c75bb
StlMaris123 <stellamarisnjage@gmail.com>
2017-11-10 11:16:20 +0300

    update gibbon configuration

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,5 @@
-Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"] 
-Gibbon::API.timeout = 15
-Gibbon::API.throw_exceptions = true
+Gibbon::Request.api_key = ENV["MAILCHIMP_API_KEY"]
+Gibbon::Request.timeout = 15
+Gibbon::Request.open_timeout = 15
+Gibbon::Request.symbolize_keys = true
+Gibbon::Request.debug = false
