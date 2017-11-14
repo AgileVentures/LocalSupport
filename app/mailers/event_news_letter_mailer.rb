79479b7bec99c77f3bf1cb59ab4388ac2256818d
StlMaris123 <stellamarisnjage@gmail.com>
2017-11-14 09:04:32 +0300

    fix ruboxop issues

--- app/mailers/event_news_letter_mailer.rb
+++ app/mailers/event_news_letter_mailer.rb
@@ -1,11 +1,11 @@
 class EventNewsLetterMailer < ApplicationMailer
-  default from: "support@harrowcn.org.uk",
-          cc: "technical@harrowcn.org.uk",
-          reply_to: "support@harrowcn.org.uk"
+  default from: 'support@harrowcn.org.uk',
+          cc: 'technical@harrowcn.org.uk',
+          reply_to: 'support@harrowcn.org.uk'
 
-  def monthly_newsletter(user, event)
+  def monthly_newsletter(user, events)
     @user = user
-    @event = event
+    @events = events
     mail to: user.email, subject: 'Upcoming events updates'
   end
 end
