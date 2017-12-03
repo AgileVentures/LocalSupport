6ef5087d1b2663e8fc7c617f3873a4abbe7e1fc0
Brent Vardy <brent.vardy@challengelogic.net>
2017-12-03 16:21:19 +0000

    refactor to fix clode climate errors

--- app/controllers/click_throughs_controller.rb
+++ app/controllers/click_throughs_controller.rb
@@ -1,17 +1,11 @@
 class ClickThroughsController < ApplicationController
   def go_to
     click_through = ClickThrough.new
-    if user_signed_in?
-      click_through.user_id = current_user.id
-    end
+    click_through.user_id = current_user.id if user_signed_in?
     click_through.source_url = request.headers['HTTP_REFERER']
-
-    if params[:url].nil?
-      click_through.url = '#'
-    else
-      click_through.url = params[:url]
-    end
+    click_through.url = params[:url].nil? ? '#' : params[:url]
     click_through.save
-    redirect_to params[:url]
+
+    redirect_to click_through.url
   end
 end
