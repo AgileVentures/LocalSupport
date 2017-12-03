27a88e6db1d745a05a5a700f9926f5181d308ac3
Brent Vardy <brent.vardy@challengelogic.net>
2017-12-03 15:12:48 +0000

    add click_through_contorller and new go_to action

---
+++ app/controllers/click_throughs_controller.rb
@@ -0,0 +1,17 @@
+class ClickThroughsController < ApplicationController
+  def go_to
+    click_through = ClickThrough.new
+    if user_signed_in?
+      click_through.user_id = current_user.id
+    end
+    click_through.source_url = request.original_url
+
+    if params[:url].nil?
+      click_through.url = '#'
+    else
+      click_through.url = params[:url]
+    end
+    click_through.save
+    redirect_to params[:url]
+  end
+end
