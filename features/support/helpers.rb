a4b6284586b9cc56418c3151fc03e43c4fa791ed
nisevi <nicolas.s.vidal@gmail.com>
2017-12-07 00:02:09 +0100

    Capybara config.

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -25,7 +25,7 @@ module Helpers
 
   # http://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
   def wait_for_ajax
-    Timeout.timeout(Capybara.default_wait_time) do
+    Timeout.timeout(Capybara.default_max_wait_time) do
       loop until finished_all_ajax_requests?
     end
   end
