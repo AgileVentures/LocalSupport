8884b08dff9946bcf5b7975580c713736f47bb5e
nisevi <nicolas.s.vidal@gmail.com>
2017-12-07 21:11:29 +0100

    Updated 'volunteer_ops_spec.rb' and 'authentication_helper.rb'.

--- spec/requests/volunteer_ops_spec.rb
+++ spec/requests/volunteer_ops_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
-describe 'VolunteerOps', :type => :request, :helpers => :requests do
+describe 'VolunteerOps', type: :request, helpers: :requests do
   let(:org_owner) { FactoryBot.create(:user_stubbed_organisation) }
-  let(:non_org_owner) { FactoryBot.create :user , :email => 'regularjoe@blah.com'}
-  let(:superadmin) {FactoryBot.create :user, :email => "superadmin@superadmin.com", :superadmin => true}
+  let(:non_org_owner) { FactoryBot.create :user , email: 'regularjoe@blah.com'}
+  let(:superadmin) {
+    FactoryBot.create :user,
+    email: 'superadmin@superadmin.com',
+    superadmin: true
+  }
 
   describe 'POST /volunteer_ops' do
     let(:params) { { volunteer_op: {title: 'hard work', description: 'for the willing'} } }
@@ -12,14 +16,14 @@ describe 'VolunteerOps', :type => :request, :helpers => :requests do
       org_admin = org_owner
       login(org_admin)
       expect {
-        post organisation_volunteer_ops_path(org_admin.organisation), params
+        post organisation_volunteer_ops_path(org_admin.organisation), params: params
       }.to change(VolunteerOp, :count).by(1)
     end
 
     it 'the new VolunteerOp is associated with the requested organisation' do
       org_admin = org_owner
       login(org_admin)
-      post organisation_volunteer_ops_path(org_admin.organisation), params
+      post organisation_volunteer_ops_path(org_admin.organisation), params: params
       op = VolunteerOp.last
       expect(op.organisation).to eq org_admin.organisation
     end
@@ -27,14 +31,14 @@ describe 'VolunteerOps', :type => :request, :helpers => :requests do
     it 'does not work for non-org-owners' do
       login(non_org_owner)
       expect {
-        post organisation_volunteer_ops_path(org_owner.organisation.id), params
+        post organisation_volunteer_ops_path(org_owner.organisation.id), params: params
       }.to change(VolunteerOp, :count).by(0)
     end
 
     it 'does work for superadmins' do
       login(superadmin)
       expect{
-        post organisation_volunteer_ops_path(org_owner.organisation.id), params
+        post organisation_volunteer_ops_path(org_owner.organisation.id), params: params
       }.to change(VolunteerOp, :count).by(1)
     end
   end
--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -17,7 +17,8 @@ end
 
 module RequestHelpers
   def login(user)
-    post user_session_path, 'user[email]' => user.email, 'user[password]' => user.password
+    post user_session_path,
+         params: {'user[email]' => user.email, 'user[password]' => user.password}
     follow_redirect!
   end
 end
