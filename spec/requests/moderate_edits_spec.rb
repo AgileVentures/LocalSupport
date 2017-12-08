e2e287530a9d3e60c0360bab3fb8a6f76744fde5
nisevi <nicolas.s.vidal@gmail.com>
2017-12-08 21:47:45 +0100

    Updated 'moderate_edits_spec.rb'.

--- spec/requests/moderate_edits_spec.rb
+++ spec/requests/moderate_edits_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
-describe "Proposed Edits Moderation", :type => :request, :helpers => :requests do
+describe 'Proposed Edits Moderation', type: :request, helpers: :requests do
   let(:nonsuperadmin) { FactoryBot.create(:user, superadmin: false) }
-  let(:org){FactoryBot.create(:organisation, name: "Friendly Organisation")}
-  let(:proposed_edit){FactoryBot.create(:proposed_organisation_edit, name: "Different Name", organisation: org)}
+  let(:org){FactoryBot.create(:organisation, name: 'Friendly Organisation')}
+  let(:proposed_edit){FactoryBot.create(:proposed_organisation_edit, name: 'Different Name', organisation: org)}
   before { login(nonsuperadmin) }
   it 'nonsuperadmin cannot accept edit' do
     expect{
-      patch organisation_proposed_organisation_edit_path(proposed_edit.organisation, proposed_edit),
-        :proposed_organisation_edit => {name: "Different Name", id: proposed_edit.id}
+      patch organisation_proposed_organisation_edit_path(
+                proposed_edit.organisation, proposed_edit
+            ),
+            params: {
+                proposed_organisation_edit: {
+                    name: 'Different Name',
+                    id: proposed_edit.id}
+            }
       org.reload
     }.not_to change{org.name}
   end
