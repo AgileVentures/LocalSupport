46b109a383e79bb9391ec15d65d979f9f77112f5
Brent Vardy <brent.vardy@challengelogic.net>
2017-12-14 14:30:32 +0000

    update maps popups to use new link_to_and_track helper to track clicks

--- app/helpers/volunteer_ops_helper.rb
+++ app/helpers/volunteer_ops_helper.rb
@@ -5,7 +5,13 @@ module VolunteerOpsHelper
 
   def link_to_vol_op(obj, type, html_options = {})
     html_options[:target] = '_blank' unless obj.source == 'local'
-    return link_to(obj.title, obj.link, html_options) if type == :title
-    link_to(obj.organisation_name, obj.organisation_link, html_options)
+
+    if obj.source == 'local'
+      return link_to_and_track(obj.title, volunteer_op_url(obj), html_options) if type == :title
+      link_to_and_track(obj.organisation_name, organisation_url(obj), html_options)
+    else
+      return link_to_and_track(obj.title, obj.link, html_options) if type == :title
+      link_to_and_track(obj.organisation_name, obj.organisation_link, html_options)
+    end
   end
 end
