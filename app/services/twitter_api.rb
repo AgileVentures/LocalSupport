31d2548158b573c771cb67f016b30e28c97d0c3e
Nick Schimek <cfme00@gmail.com>
2017-12-19 00:02:17 -0800

    Update twitter ENV var names

--- app/services/twitter_api.rb
+++ app/services/twitter_api.rb
@@ -6,7 +6,7 @@ class TwitterApi
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
   end
 
