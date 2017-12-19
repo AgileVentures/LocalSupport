14f126ada6e015ef9ae797b3e69bce882894fa94
Nick Schimek <cfme00@gmail.com>
2017-12-19 07:07:42 -0800

    Push up to troubleshoot
    twitter-text gem parse_tweet method is returning undefined method for
    nil class

--- app/services/twitter_api.rb
+++ app/services/twitter_api.rb
@@ -1,4 +1,5 @@
 class TwitterApi
+  include Twitter::Validation
   attr_reader :client
 
   def initialize
@@ -10,12 +11,14 @@ class TwitterApi
     end
   end
 
-  def post_to_twitter(msg)
-    client.update(tweet_size(msg))
+  def post_to_twitter(msg, url = "https://www.harrowcn.org.uk/")
+   # parse_results = parse_tweet(msg + " " + url)
+   # tweet = msg[parse_results.display_range_start...parse_results.display_range_end]
+   # parse_results = parse_tweet("test test test")
+   # client.update("test test test 1") if parse_results.valid
+   tweet = msg + " " + url
+   byebug
+   result = parse_tweet(tweet)
+   client.update(msg[result.display_range_start...result.display_range_end] + " " + url)
   end
-
-  def tweet_size(msg)
-    msg[0..139]
-  end
-
 end
--- spec/services/twitter_api_spec.rb
+++ spec/services/twitter_api_spec.rb
@@ -11,7 +11,7 @@ describe TwitterApi do
   end
 
   context 'Posting to twitter' do
-    it 'should be able to post 140 chars or less to twitter' do
+    it 'should be able to post 280 chars or less to twitter' do
       number_of_tweets = @twitter_client.client.user.tweets_count
       tweet = "Test Tweet #{number_of_tweets}"
       @twitter_client.post_to_twitter(tweet)
@@ -19,10 +19,11 @@ describe TwitterApi do
       expect(@twitter_client.client.user.tweets_count).to eq(number_of_tweets + 1)
     end
 
-    it 'should be able to truncate longer messages to 140 chars and post to twitter' do
-      tweet = "This string is going to be greater than 140 characters so we can test that our truncate method is functioning correctly. This is number 140. All of these characters should be truncated."
-
-      expect(@twitter_client.tweet_size(tweet).length).to eq(140)
+    it 'should be able to post 280 chars or more to twitter' do
+      number_of_tweets = @twitter_client.client.user.tweets_count
+      tweet = "Test Tweet #{number_of_tweets} This string is going to be greater than 280 characters so we can test that our truncate method is functioning correctly. This is number 140. This string is going to be greater than 280 characters so we can test that our truncate method is functioning correctly. This is number 280. All of these characters should be truncated."
+      @twitter_client.post_to_twitter(tweet)
+      expect(@twitter_client.client.user.tweets_count).to eq(number_of_tweets + 1)
     end
 
   end
