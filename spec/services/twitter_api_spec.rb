1d490dd3710408fee70e8f68bca207d9bf9e47a2
Nick Schimek <cfme00@gmail.com>
2017-12-24 21:55:26 -0800

    Configure a basic stub_request

--- spec/services/twitter_api_spec.rb
+++ spec/services/twitter_api_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
   context 'Posting to twitter' do
+    twitter_client = TwitterApi.new()
+    desc = "Volunteer opportunity"
+    long_tweet = "a" * 500
     it 'should be able to post 280 chars or less to twitter' do
-      #number_of_tweets = @twitter_client.user.tweets_count
-
-      @twitter_client.post_to_twitter("test 100110")
-
-      #expect(@twitter_client.user.tweets_count).to eq(number_of_tweets + 1)
+      stub_request(:post, "https://api.twitter.com/1.1/statuses/update.json").
+         with(body: {"status"=>"New #volunteer opportunity at https://www.harrowcn.org.uk/ - #{desc}"},
+              headers: {}).
+         to_return(status: 200, body: "", headers: {})
     end
 
     it 'should be able to post 280 chars or more to twitter' do
