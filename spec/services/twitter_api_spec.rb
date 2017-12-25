d2b727edf8c3d368410dd1098887735aad462483
Nick Schimek <cfme00@gmail.com>
2017-12-24 16:17:07 -0800

    Implement temporary test that hits API
    This is to make sure everything works and appears as expected.

--- spec/services/twitter_api_spec.rb
+++ spec/services/twitter_api_spec.rb
@@ -1,29 +1,30 @@
 require 'rails_helper'
 
+
 describe TwitterApi do
   before(:all) do
     @twitter_client = TwitterApi.new()
-    WebMock.allow_net_connect!
+    WebMock.disable! #TODO: Will need to implement a stub request
   end
 
   after(:all) do
-    WebMock.disable_net_connect!
+    WebMock.enable! #TODO: Will need to implement a stub request
   end
 
   context 'Posting to twitter' do
     it 'should be able to post 280 chars or less to twitter' do
-      number_of_tweets = @twitter_client.client.user.tweets_count
-      tweet = "Test Tweet #{number_of_tweets}"
-      @twitter_client.post_to_twitter(tweet)
+      #number_of_tweets = @twitter_client.user.tweets_count
 
-      expect(@twitter_client.client.user.tweets_count).to eq(number_of_tweets + 1)
+      @twitter_client.post_to_twitter("test 100110")
+
+      #expect(@twitter_client.user.tweets_count).to eq(number_of_tweets + 1)
     end
 
     it 'should be able to post 280 chars or more to twitter' do
-      number_of_tweets = @twitter_client.client.user.tweets_count
-      tweet = "Test Tweet #{number_of_tweets} This string is going to be greater than 280 characters so we can test that our truncate method is functioning correctly. This is number 140. This string is going to be greater than 280 characters so we can test that our truncate method is functioning correctly. This is number 280. All of these characters should be truncated."
+   #   number_of_tweets = @twitter_client.client.user.tweets_count
+      tweet = "Test Tweet This string is going to be greater than 280 characters so we can test that our truncate method is functioning correctly. This is number 140. This string is going to be greater than 280 characters so we can test that our truncate method is functioning correctly. This is number 280. All of these characters should be truncated."
       @twitter_client.post_to_twitter(tweet)
-      expect(@twitter_client.client.user.tweets_count).to eq(number_of_tweets + 1)
+   #   expect(@twitter_client.client.user.tweets_count).to eq(number_of_tweets + 1)
     end
 
   end
