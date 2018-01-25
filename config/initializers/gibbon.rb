80fcc4aa78c4efa94d3c3951a3eac4a040faa1a9
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 09:55:13 +0100

    Gibbon configuration file.

---
+++ config/initializers/gibbon.rb
@@ -0,0 +1,5 @@
+Gibbon::Request.api_key = ENV['GIBBON_API_KEY']
+Gibbon::Request.timeout = 15
+Gibbon::Request.open_timeout = 15
+Gibbon::Request.symbolize_keys = true
+Gibbon::Request.debug = false
