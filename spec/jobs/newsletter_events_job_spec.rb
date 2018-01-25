bae9caee3a89204591a351bc6ba7740d5c8e4ced
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 13:27:36 +0100

    Removed spec for newsletter events.

--- spec/jobs/newsletter_events_job_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe NewsletterEventsJob, type: :job do
-  describe '#perform' do
-    it 'creates a campaign based on organisation emails' do
-
-    end
-  end
-end
