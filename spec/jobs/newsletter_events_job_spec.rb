51b0810585a46e5e2001aac08711ead339593e58
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 11:11:52 +0100

    Placeholder for the job we want to run monthly.
    Descriptions of the steps we need to follow in order to accomplish the required functionality.
    
    Spec for testing the job was created.

---
+++ spec/jobs/newsletter_events_job_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe NewsletterEventsJob, type: :job do
+  describe '#perform' do
+    it 'creates a campaign based on organisation emails' do
+
+    end
+  end
+end
