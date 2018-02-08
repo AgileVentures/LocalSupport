cc7d3de3a03969cf72a1902d7484311aed4b4aa3
GettyGertie <gettyorawo@gmail.com>
2018-02-08 13:05:22 +0300

    added a comment

--- app/helpers/volunteer_ops_helper.rb
+++ app/helpers/volunteer_ops_helper.rb
@@ -16,6 +16,7 @@ module VolunteerOpsHelper
       link_to_and_track(obj.organisation_name,
                         organisation_url(obj.organisation_link.slug),
                         html_options)
+      #commmeeennt
     end
   end
 
