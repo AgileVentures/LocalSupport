6c122fc831792a0a47f4772bfd5b082383d7e854
kevin <kevinochieng548@gmail.com>
2018-02-08 13:13:35 +0300

    generate event time migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108141814) do
+ActiveRecord::Schema.define(version: 20180208101118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,13 @@ ActiveRecord::Schema.define(version: 20180108141814) do
     t.text "description"
     t.datetime "start_date"
     t.datetime "end_date"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "organisation_id"
-    t.float    "latitude"
-    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "organisation_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "features", id: :serial, force: :cascade do |t|
