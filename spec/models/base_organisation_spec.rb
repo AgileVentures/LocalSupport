319b9ef3a1c54ebbcb4020ec0730a4809a064976
Nicolas Sebastian Vidal <nicolas.s.vidal@gmail.com>
2018-02-08 17:57:47 +0100

    Geocoder logic moved to be applied on the OrganisationsController. (#618)
    * Geocoder logic moved to be applied on the OrganisationsController.
    
    Tests on BaseOrganisation and Organisation models related to the callback to geocoder gem were removed since we have to create acceptance tests for covering this functionality.
    
    * Geocoding before saving imported organization.
    
    * Understanding @vcr.
    
    * Organisations need to be geocoded when testing.
    
    * Geocoding before saving.
    
    * Attributes are being updated and saved explicitly.
    
    Tests were updated accordingly.
    
    * Doing a little refactoring on Organisation model and OrganisationsController.
    
    * We don't test private methods :D !
    
    * Refactoring on OrganisationsController due to codeclimate complains.
    
    * Codeclimate complain -> Assignment Branch Condition size for add_email is too high. [16.4/15]
    
    * Refactoring...o.O?
    
    * Codeclimate O.O'
    
    * It was the other way around.
    
    * Guard clause instead of wrapping the code inside a conditional.

--- spec/models/base_organisation_spec.rb
+++ spec/models/base_organisation_spec.rb
@@ -34,64 +34,11 @@ describe BaseOrganisation, type: :model do
     end
   end
 
-  describe 'geocoding object that already exists' do
-    subject { FactoryBot.create(:organisation, updated_at: 366.days.ago) }
-    it 'should geocode when address changes' do
-      new_address = '30 pinner road'
-      is_expected.to receive(:geocode)
-      subject.update_attributes address: new_address
-      end
-
-    it 'should geocode when postcode changes' do
-      new_postcode = 'HA1 4RZ'
-      is_expected.to receive(:geocode)
-      subject.update_attributes postcode: new_postcode
-    end
-  end
-
-  describe 'geocoding object that is being created' do
-    it 'should not geocode if coordinates are setup and I have an address' do
-      postcode = 'HA1 4HZ'
-      address = '60 pinner road'
-      org = FactoryBot.build(
-          :organisation,
-          address: address,
-          latitude: Faker::Address.latitude,
-          longitude: Faker::Address.longitude,
-          postcode: postcode,
-          name: 'Happy and Nice',
-          gmaps: true
-      )
-          expect(org).to_not receive(:geocode)
-          org.save
-    end
-
-    it 'should geocode if coordinates are not setup and I have an address' do
-      postcode = 'HA1 4HZ'
-      address = '60 pinner road'
-      org = FactoryBot.build(
-          :organisation,
-          address: address,
-          latitude: nil,
-          longitude: nil,
-          postcode: postcode,
-          name: 'Happy and Nice',
-          gmaps: true
-      )
-      expect(org).to receive(:geocode)
-      org.save
-    end
-  end
-  
   describe 'friendly_id' do
-     
-     it 'should use SetupSlug service for setting slugs' do
-       service_double = class_double(SetupSlug).as_stubbed_const
-       expect(service_double).to receive(:run)
-       
-       FactoryBot.create(:organisation)
-     end
-     
+    it 'should use SetupSlug service for setting slugs' do
+      service_double = class_double(SetupSlug).as_stubbed_const
+      expect(service_double).to receive(:run)
+      FactoryBot.create(:organisation)
+    end
   end
-
 end
