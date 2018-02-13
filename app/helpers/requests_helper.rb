e381f7bc8da90af5388695b2d38b0f670398ae5c
GettyGertie <gettyorawo@gmail.com>
2018-02-13 14:38:24 +0300

    Fixed 'def' and 'end' indentation

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,6 +1,6 @@
 module RequestsHelper
-	
-def send_email_to_superadmin_about_request_for_admin_of org
+
+  def send_email_to_superadmin_about_request_for_admin_of org
     superadmin_emails = User.superadmins.pluck(:email)
     AdminMailer.new_user_waiting_for_approval(org.name, superadmin_emails).deliver_now
   end
