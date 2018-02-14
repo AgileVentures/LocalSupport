894f52bc71efc1a61379b440939c724e3182ea0e
nisevi <nicolas.s.vidal@gmail.com>
2018-02-14 15:25:38 +0100

    Created spec file and pending example tests.

---
+++ spec/helpers/organisations_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe OrganisationsHelper, type: :helper do
+  let(:organisation) { FactoryBot.create(:organisation) }
+
+  it 'should render link categories'
+
+  it 'should return the correct hash'
+end
