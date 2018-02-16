7846e2dd0611f4918b86776a34b22eedda5fb52c
GettyGertie <gettyorawo@gmail.com>
2018-02-16 13:54:51 +0300

    Add fields to Volunteer_ops table

---
+++ db/migrate/20180216103915_add_fields_to_volunteer_ops.rb
@@ -0,0 +1,9 @@
+class AddFieldsToVolunteerOps < ActiveRecord::Migration[5.1]
+  def change
+    add_column :volunteer_ops, :role_description, :string
+    add_column :volunteer_ops, :skills_needed, :string
+    add_column :volunteer_ops, :when_volunteer_needed, :boolean, default: false
+    add_column :volunteer_ops, :contact_details, :string
+    add_column :volunteer_ops, :about_organization, :string
+  end
+end
