abd9011ad3c05ff018f28bc3486c6b9fbc2fa851
musaomondi <amokem@yahoo.com>
2018-02-21 14:12:19 +0300

    remove event start and end time columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208101118) do
+ActiveRecord::Schema.define(version: 20180221110841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,6 @@ ActiveRecord::Schema.define(version: 20180208101118) do
     t.integer "organisation_id"
     t.float "latitude"
     t.float "longitude"
-    t.datetime "start_time"
-    t.datetime "end_time"
   end
 
   create_table "features", id: :serial, force: :cascade do |t|
