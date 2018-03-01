73f97c840e71e3dafcbe203c7ae835756cdbcab8
Nick Schimek <cfme00@gmail.com>
2018-03-01 13:41:32 -0800

    Update spec to test doit volops are tweeted
    Currently only doit volops are tweeted through update_social_media.rb
    once reachskills api has been updated we should be able to persist
    accurate 'created at' data to also tweet new reachskills ops.
    
    Local volops are tweeted when they are created through the admin page.
    Local volops should not be retweeted when update_social_media.rb
    processes.

--- spec/services/update_social_media_spec.rb
+++ spec/services/update_social_media_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-describe UpdateSocialMedia do
+RSpec.describe UpdateSocialMedia do
+  before :each do
+    organisation = FactoryBot.create :organisation, name: 'Friendly'
+    FactoryBot.create :doit_volunteer_op, source: 'doit'
+    FactoryBot.create :doit_volunteer_op, source: 'doit'
+    FactoryBot.create :doit_volunteer_op, source: 'doit', created_at: 1.day.ago
+    FactoryBot.create :local_volunteer_op, source: 'local', organisation: organisation
+  end
 
+  context 'Posting to Twitter' do
+    # Currently reachskills ops are not tweeted due to api constraints
+    # See Pivotal Tracker story: https://www.pivotaltracker.com/story/show/153805125
+    it 'tweets only new doit ops' do
+      expect_any_instance_of(UpdateSocialMedia).to receive(:post).exactly(2).times
+      UpdateSocialMedia.new.post_new_volops_from_partner_sites
+    end
+  end
 end
