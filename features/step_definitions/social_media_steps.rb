41b90fb747c584211a574ba0dd13a21c9e27ee71
Nick Schimek <cfme00@gmail.com>
2018-03-01 09:11:10 -0800

    Add test to expect new doit ops are tweeted

--- features/step_definitions/social_media_steps.rb
+++ features/step_definitions/social_media_steps.rb
@@ -3,17 +3,7 @@ Given /^I run the update social media task$/ do
 end
 
 Then(/^there should be (\d+) posts to twitter$/) do |number_posts|
-  # https://github.com/bblimke/webmock#setting-expectations-in-rspec-on-webmock-module
 
-  # expect(WebMock).to have_requested(:post, 'https://api.twitter.com/1.1/statuses/update.json', times: number_posts)
+  expect_any_instance_of(TwitterApi).to receive(:tweet).exactly(number_posts.to_i).times
 
-  expect(a_request(:post, 'https://api.twitter.com/1.1/statuses/update.json')).
-      to have_been_made.times(number_posts)
 end
-
-
-# expect(WebMock).to have_requested(:post, 'https://api.twitter.com/1.1/statuses/update.json').
-#       with { |req| req.body =~ /hangout\.test/ }
-
-# expect(WebMock).to have_requested(:post, 'https://api.twitter.com/1.1/statuses/update.json').
-#       with { |req| req.body =~ /hangout\.test/ }
