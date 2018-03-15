ca25fcb10c9f151642a860c9608c12db85c145c7
nisevi <nicolas.s.vidal@gmail.com>
2018-03-15 14:47:42 +0100

    Fix jasmine spec.

--- spec/javascripts/bootstrap_sortable_rails_spec.js
+++ spec/javascripts/bootstrap_sortable_rails_spec.js
@@ -1,9 +1,10 @@
 describe('gem bootstrap_sortable_rails', function() {
     var header;
     beforeEach(function() {
-        setFixtures('<table class="sortable"><thead><tr><th data-sortkey="0">Name</th></tr></thead>' +
-            '<tbody><tr><td data-value="Harrow Baptist Church"><a href="/organisations/1">Harrow Baptist Church</td></tr>' +
-            '<tr><td data-value="Human Touch Worldwide"><a href="/organisations/1">Human Touch Worldwide</td></tr></tbody></table>');
+        setFixtures('<table class="sortable"><thead><tr><th data-sortkey="0">Name</th></tr></thead><tbody>')
+        appendSetFixtures('<tr><td data-value="Harrow Baptist Church"><a href="/organisations/1">Harrow Baptist Church</td></tr>')
+        appendSetFixtures('<tr><td data-value="Human Touch Worldwide"><a href="/organisations/1">Human Touch Worldwide</td></tr>')
+        appendSetFixtures('</tbody></table>')
         header = $('th');
     });
     describe('when clicking on the tableheader', function() {
@@ -11,7 +12,7 @@ describe('gem bootstrap_sortable_rails', function() {
             header.click();
             expect($('td').first()).toHaveText('Harrow Baptist Church');
             header.click();
-            expect($('td').first()).toHaveText('Human Touch Worldwide')
+            expect($('td').last()).toHaveText('Human Touch Worldwide')
         });
         it('inserts an arrow after click', function() {
             header.click();
