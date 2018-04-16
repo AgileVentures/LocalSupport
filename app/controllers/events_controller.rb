0008cc384a4e9c1ab854e0dfb2bcd4d97088a61d
Mariam Ahhttouche <maryam.ahhtouch@gmail.com>
2018-04-16 07:40:54 +0000

    Add address to controller event params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,7 +37,7 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:title, :description,
-      :start_date, :end_date, :organisation_id)
+      :start_date, :end_date, :organisation_id, :address)
   end
 
   def logged_in_user
