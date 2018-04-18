3bf15f759d79830a3db9e49366f17c1f31fca71b
Brent Vardy <brent.vardy@challengelogic.net>
2018-04-18 16:06:19 +0100

    improvements to import_organisations tests

--- app/services/import_organisations.rb
+++ app/services/import_organisations.rb
@@ -1,4 +1,16 @@
 class ImportOrganisations
-  def self.with(postcode)
+  def self.with(postcode = 'HA2',
+      http = HTTParty,
+      model_klass = Organisation)
+
+      response = http.get('http://production.charity-api.agileventures.org/charities.json?postcode=ha2')
+
+      model_klass.find_or_create_by if has_content?(response)
   end
+
+
+  def self.has_content?(response)
+    response.body && response.body != '[]'
+  end
+
 end
