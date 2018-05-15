dbb9422328e3e9b2c4d0252da39d0ac1567148e6
Mariam Ahhttouche <maryam.ahhtouch@gmail.com>
2018-05-15 08:15:51 +0000

    Make migration reversible

--- db/migrate/20171228144437_update_user_id_for_click_throughs.rb
+++ db/migrate/20171228144437_update_user_id_for_click_throughs.rb
@@ -1,5 +1,9 @@
 class UpdateUserIdForClickThroughs < ActiveRecord::Migration[4.2]
-  def change
+  def up
     change_column :click_throughs, :user_id, 'integer USING CAST(user_id AS integer)'
+  end
+  
+  def down
+    change_column :click_throughs, :user_id
   end
 end
