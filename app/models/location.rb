f1ba5ea6665cce63e63e1c40ba0796bff549fbe6
Nick Schimek <cfme00@gmail.com>
2018-06-02 00:08:25 -0700

    Add constants for central harrow location

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,13 +6,16 @@ class Location
     @longitude = longitude
     @latitude = latitude
   end
-  
+
+  CENTRAL_HARROW_LATITUDE = 51.58056
+  CENTRAL_HARROW_LONGITUDE = -0.34199
+
   def ==(other)
     self.class == other.class &&
       self.longitude == other.longitude &&
       self.latitude == other.latitude
   end
-  
+
   alias eql? ==
 
 
