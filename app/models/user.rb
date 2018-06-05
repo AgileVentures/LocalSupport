b241f7517c80bc7c9c385c040f04b0e9722b37a7
StlMaris123 <stellamarisnjage@gmail.com>
2018-06-05 14:56:21 +0300

    provide default organisation in event creation if a user has an organization [Finishes #157115605]

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   # prevents mass assignment on other fields not in this list
   #attr_accessible :email, :password, :password_confirmation, :remember_me, :pending_organisation_id
   belongs_to :organisation
-  belongs_to :pending_organisation, class_name: :organisation, foreign_key: :pending_organisation_id
+  belongs_to :pending_organisation, class_name: 'Organisation', foreign_key: :pending_organisation_id
 
   # should we have a before_save here where we check if the pending_organization_id is going from
   # nil to a value and then send the superadmin an email ...
