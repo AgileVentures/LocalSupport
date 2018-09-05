aa9fc0aaf2a921113a81ad65a5705e8550160be4
Matt Rider <mattwr18@gmail.com>
2018-09-05 19:57:49 -0300

    Configure shoulda matchers to get tests passing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,3 +75,10 @@ RSpec.configure do |config|
 
   config.before(:each) { ActionMailer::Base.deliveries.clear }
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
