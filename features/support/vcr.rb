a7f419f3cb62f8d45f066536e84666b24875103d
Matt Rider <mattwr18@gmail.com>
2018-09-18 20:53:37 -0300

    Automate vcr renewal, renew vcr cassettes

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -3,7 +3,10 @@ VCR.configure do |config|
   config.cassette_library_dir = "features/vcr_cassettes"
   config.hook_into :webmock
   config.debug_logger = File.open('vcr.log', 'w')
-  config.default_cassette_options = { record: :new_episodes }
+  config.default_cassette_options = {
+    clean_outdated_http_interactions: true,
+    re_record_interval: 1.month.to_i,
+    record: :new_episodes }
   config.ignore_request do |request|
     request.headers.include?('Referer')
   end
