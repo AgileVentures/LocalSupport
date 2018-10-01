ca3ef98861af481d752efd172f0a923820e69b45
Matt Rider <mattwr18@gmail.com>
2018-10-01 13:54:40 -0300

    Move encrypted key to root

--- travis_deploy_to_dokku_encryption.sh
+++ travis_deploy_to_dokku_encryption.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_eee247762962_key -iv $encrypted_eee247762962_iv -in .travis/deploy.key.enc -out .travis/deploy.key -d
+openssl aes-256-cbc -K $encrypted_eee247762962_key -iv $encrypted_eee247762962_iv -in deploy.key.enc -out .travis/deploy.key -d
 exit 0
