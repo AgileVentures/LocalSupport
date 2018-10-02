21ebd9733600ce7781d7a2750bedbcc72452f49d
Matt Rider <mattwr18@gmail.com>
2018-10-02 12:03:52 -0300

    Add deploy script, check if parent repo pushing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,7 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
-# test ssh connection for: https://github.com/dwyl/learn-travis/issues/42
-exit 0
+ssh-keyscan agileventures-playground.westeurope.cloudapp.azure.com >> ~/.ssh/known_hosts
+git remote add develop dokku@agileventures-playground.westeurope.cloudapp.azure.com:harrowcn-develop
+git config --global push.default simple
+git push develop develop:master
