d0f721832b30e16c6a98a6821a6db5472eaaf24c
Matt Rider <mattwr18@gmail.com>
2018-10-02 09:20:03 -0300

    Modify script to check if ssh into box works

--- after_success_deploy_to_dokku.sh
+++ after_success_deploy_to_dokku.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy.key
-ssh-add .travis/deploy.key
+# eval "$(ssh-agent -s)"
+# chmod 600 .travis/deploy.key
+# echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+# ssh-add .travis/deploy.key
 ssh-keyscan agileventures-playground.westeurope.cloudapp.azure.com >> ~/.ssh/known_hosts
 git remote add develop dokku@agileventures-playground.westeurope.cloudapp.azure.com:harrowcn-develop
 git config --global push.default simple
--- travis_deploy_to_dokku_encryption.sh
+++ travis_deploy_to_dokku_encryption.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_eee247762962_key -iv $encrypted_eee247762962_iv -in deploy.key.enc -out .travis/deploy.key -d
+openssl aes-256-cbc -K $encrypted_a6fc752c6b5a_key -iv $encrypted_a6fc752c6b5a_iv -in deploy_key.enc -out ./deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ./deploy_key
+echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deploy_key
+# test ssh connection for: https://github.com/dwyl/learn-travis/issues/42
+ssh -i ./deploy_key root@agileventures-playground.westeurope.cloudapp.azure.com pwd
 exit 0
