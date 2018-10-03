312f62a4c559e650a2fe177299bdadc21e8fc911
Matt Rider <mattwr18@gmail.com>
2018-10-03 09:13:06 -0300

    Add back ssh into box to debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
+ssh -iyv ./deploy_key root@104.40.243.49 pwd
 ssh-keyscan agileventures-playground.westeurope.cloudapp.azure.com >> ~/.ssh/known_hosts
 git remote add develop dokku@agileventures-playground.westeurope.cloudapp.azure.com:harrowcn-develop
 git config --global push.default simple
