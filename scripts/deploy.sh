b048c664f3d58d29e3218c30f24cf06d91dce17e
Matt Rider <mattwr18@gmail.com>
2018-10-03 09:39:05 -0300

    Move ssh into box below ssh-keyscan
    To identify the user and avoid strict host key checking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
-ssh -iyv ./deploy_key root@104.40.243.49 pwd
 ssh-keyscan agileventures-playground.westeurope.cloudapp.azure.com >> ~/.ssh/known_hosts
+ssh -v -i ./deploy_key root@104.40.243.49 pwd
 git remote add develop dokku@agileventures-playground.westeurope.cloudapp.azure.com:harrowcn-develop
 git config --global push.default simple
 git push develop configure_travis_to_push_to_dokku:master
