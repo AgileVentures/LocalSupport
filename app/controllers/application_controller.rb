0b740c4fdd7d6e433fb6c26830b74c5e1cbc5fd3
aonomike <aonomike@gmail.com>
2018-10-07 01:02:30 +0300

    Refactor to have two items on each line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,11 @@ class ApplicationController < ActionController::Base
   # To prevent infinite redirect loops, only requests from white listed
   # controllers are available in the "after sign-in redirect" feature
   def white_listed
-    %w( application
-        contributors
-        organisations
-        pages
-        volunteer_ops
-        events)
+    %w(
+        application contributors
+        organisations pages
+        volunteer_ops events
+      )
   end
   # Devise wiki suggests we need to make this return nil for the
   # after_inactive_signup_path_for to be called in registrationscontroller
