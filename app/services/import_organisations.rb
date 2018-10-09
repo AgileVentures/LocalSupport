32429572cd36cd392096295868a327d574ab96e0
Matt Rider <mattwr18@gmail.com>
2018-10-09 12:09:37 -0300

    Remove description validation, fix code smells

--- app/services/import_organisations.rb
+++ app/services/import_organisations.rb
@@ -1,28 +1,25 @@
 class ImportOrganisations
   def self.with(postcode = 'HA2',
-      http = HTTParty,
-      model_klass = Organisation)
+    http = HTTParty,
+    model_klass = Organisation)
 
-      response = http.get("http://production.charity-api.agileventures.org/charities.json?postcode=#{postcode}")
-      if has_content?(response)
-        charities = JSON.parse response.body
-        charities.each do |charity|
-          organisation = model_klass.find_or_create_by! name: charity['name'], description: 'No Description' 
-          organisation.update ({
-            address: charity['add1'],
-            postcode: charity['postcode'],
-            # what other data do want here?
-            # which data should be in the block?
-          })
-          organisation.update telephone: charity['phone'] unless charity['phone'].nil?
-        end
-      end
-
+    response = http.get("http://production.charity-api.agileventures.org/charities.json?postcode=#{postcode}")
+    return unless content?(response)
+    charities = JSON.parse response.body
+    charities.each do |charity|
+      organisation = model_klass.find_or_create_by! name: charity['name'] 
+      organisation.update ({
+        address: charity['add1'],
+        postcode: charity['postcode'],
+        # what other data do want here?
+        # which data should be in the block?
+      })
+      organisation.update telephone: charity['phone'] unless charity['phone'].nil?
+    end
   end
 
 
-  def self.has_content?(response)
+  def self.content?(response)
     response.body && response.body != '[]'
   end
-
 end
