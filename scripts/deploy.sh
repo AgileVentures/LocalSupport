8e471e35a6d5612bac254112ca9e082a71d1355c
Matt Rider <mattwr18@gmail.com>
2018-11-05 15:57:37 -0200

    Remove HostKeyChecking line from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@
 openssl aes-256-cbc -K $encrypted_a6fc752c6b5a_key -iv $encrypted_a6fc752c6b5a_iv -in deploy_key.enc -out ./deploy_key -d
 eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
-echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
 ssh-keyscan agileventures-playground.westeurope.cloudapp.azure.com >> ~/.ssh/known_hosts
 git remote add develop dokku@agileventures-playground.westeurope.cloudapp.azure.com:harrowcn-develop
