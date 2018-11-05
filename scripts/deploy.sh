3888d0287b1d13b2583c639d6846ef514ed2ad4d
Matt Rider <mattwr18@gmail.com>
2018-11-05 15:03:01 -0200

    Remove ssh command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
 ssh-keyscan agileventures-playground.westeurope.cloudapp.azure.com >> ~/.ssh/known_hosts
-ssh -o StrictHostKeyChecking=no -v -i ./deploy_key root@104.40.243.49 pwd
 git remote add develop dokku@agileventures-playground.westeurope.cloudapp.azure.com:harrowcn-develop
 git config --global push.default simple
 git push develop configure_travis_to_push_to_dokku:master
