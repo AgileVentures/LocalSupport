d66ad8f67b6f8aa5547957670c0fff466663650f
Cire DIA <ciremoussadia@gmail.com>
2019-04-03 15:17:07 +0000

    White label banner (#1029)
    * White label banner
    
    * add image path defaults

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -7,7 +7,9 @@ class Setting < ApplicationRecord
     meta_tag_title: 'Harrow Community Network',
     meta_tag_site: 'Harrow volunteering',
     meta_tag_description: 'Volunteering Network for Harrow Community',
-    open_graph_site: 'Harrow Community Network'
+    open_graph_site: 'Harrow Community Network',
+    large_banner_path: 'HCN-long.png',
+    small_banner_path: 'HCN-small.png'
   }
 
   def self.method_missing(args)
