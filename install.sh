1d7ab533638215b1d31b62b3587eb2f701065fe4
Matt Rider <mattwr18@gmail.com>
2019-04-03 18:28:24 -0300

    Run db:schema:load instead of migrate in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 npm install -g bower
 bower install
 bundle exec rake db:create:all
-bundle exec rake db:migrate
-bundle exec rake db:migrate VERSION=0
-bundle exec rake db:migrate
+bundle exec rake db:schema:load
+# bundle exec rake db:migrate
+# bundle exec rake db:migrate VERSION=0
+# bundle exec rake db:migrate
 bundle exec rake db:test:prepare
