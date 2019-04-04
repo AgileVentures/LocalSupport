e42da8c405fb3a2a5c5256b73ec55e0d7c3b05e6
Sam Joseph <tansaku@gmail.com>
2019-04-04 13:47:29 +0100

    try out string back in the root

--- config/routes.rb
+++ config/routes.rb
@@ -98,7 +98,7 @@ LocalSupport::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => Setting.root_path
+  root :to => 'volunteer_ops#index' # Setting.root_path
 
   # See how all your routes lay out with "rake routes"
 
