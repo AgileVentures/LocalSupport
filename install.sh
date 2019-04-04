3fab084d943ef6fef9ace06200dc1fdcff14f2e6
Sam Joseph <tansaku@gmail.com>
2019-04-04 13:39:36 +0100

    revert install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
 npm install -g bower
 bower install
 bundle exec rake db:create:all
-bundle exec rake db:schema:load
-# bundle exec rake db:migrate
-# bundle exec rake db:migrate VERSION=0
-# bundle exec rake db:migrate
+bundle exec rake db:migrate
+bundle exec rake db:migrate VERSION=0
+bundle exec rake db:migrate
 bundle exec rake db:test:prepare
