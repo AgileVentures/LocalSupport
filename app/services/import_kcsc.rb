dccb5707cbd30ed7e8b6e6a3853e9db0f1847903
Sam Joseph <tansaku@gmail.com>
2019-05-06 17:03:33 +0100

    first pass on grabbing kcsc contacts

--- app/services/import_kcsc.rb
+++ app/services/import_kcsc.rb
@@ -1,2 +1,37 @@
 class ImportKCSC
+  def self.with(http = HTTParty, model_klass = Organisation)
+    new(http, model_klass).send(:run)
+  end
+
+  private
+
+  attr_reader :http
+
+  def initialize http, model_klass
+    @http = http
+    @model_klass = model_klass
+  end
+
+  HOST = 'https://www.kcsc.org.uk'
+  HREF = '/api/self-care/all?key='
+
+  def run
+    @response = http.get "#{HOST}#{HREF}#{ENV['KCSC_API_KEY']}"
+    return unless response_has_content?
+    @kcsc_contacts = JSON.parse(@response.body)['organisations']
+    find_or_create_organisations
+  end
+
+  def response_has_content?
+    @response.body && @response.body != '{}'
+  end
+
+  def find_or_create_organisations
+    @kcsc_contacts.each do |kcsc_contact|
+      name = kcsc_contact['organisation']['Delivered by-Organization Name'].titleize
+      description = kcsc_contact['organisation']['Summary of Activities']
+      organisation = @model_klass.find_or_create_by! name: name, description: description
+    end
+  end
+
 end
--- spec/services/import_kcsc_spec.rb
+++ spec/services/import_kcsc_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
 describe ImportKCSC do
+  let(:http_party) { double :http_party }
+  let(:url) {"#{ImportKCSC::HOST}#{ImportKCSC::HREF}#{ENV['KCSC_API_KEY']}"}
+  let(:model_klass) { spy :model_klass }
+
+  subject(:import_kcsc) { described_class.with(http_party, model_klass) }
+
+  context 'nothing to import' do
+    let(:response) {double :response, body: '{}'} 
+
+    it 'does request data from the api' do
+      expect(http_party).to receive(:get).with(url).and_return(response)
+      import_kcsc
+    end
+
+    it 'does not create any organisations' do
+      allow(http_party).to receive(:get).and_return(response)
+      import_kcsc
+      expect(model_klass).not_to have_received(:find_or_create_by!) 
+    end
+  end
+  
+  context 'list to import is returned' do
+    let(:json_file) {'test/fixtures/kcsc_self_care.json'}
+    let(:response) { double :response, body: File.read(json_file) }
+
+    it 'checks for presence and/or creates 2 organisations' do
+      allow(http_party).to receive(:get).and_return(response)
+      import_kcsc
+      expect(model_klass).to have_received(:find_or_create_by!).exactly(2).times
+    end
+  end
 end
