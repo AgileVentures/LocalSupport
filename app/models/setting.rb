2fb6c45be8434aa0b4868b1d5c376cf121a6f60c
Sam Joseph <tansaku@gmail.com>
2019-05-22 17:10:03 +0100

    various additional settings related to white labelling

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,15 +1,33 @@
+# == Schema Information
+#
+# Table name: settings
+#
+#  id         :bigint           not null, primary key
+#  key        :string
+#  value      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Setting < ApplicationRecord
   
   DEFAULTS = {
     latitude: '51.5978',
     longitude: '-0.3370',
+    site_provider: 'Voluntary Action Harrow Cooperative',
+    site_provider_url: 'http://www.voluntaryactionharrow.org.uk/',
     site_title: 'Harrow Community Network',
+    site_brand: 'Search for local voluntary and community organisations',
+    site_locale: 'Harrow',
+    site_key_asset_type: 'volunteer opportunities',
+    site_stakeholders: 'local residents and organisations',
     meta_tag_title: 'Harrow Community Network',
     meta_tag_site: 'Harrow volunteering',
     meta_tag_description: 'Volunteering Network for Harrow Community',
     open_graph_site: 'Harrow Community Network',
     large_banner_path: 'HCN-long.png',
-    small_banner_path: 'HCN-small.png',
+    small_banner_path: 'HCN-small.png', 
+    site_provider_small_logo: 'VAHC-Small-Logo.png',
     map_zoom_level: '12',
     embedded_map_zoom_level: '14'
   }
