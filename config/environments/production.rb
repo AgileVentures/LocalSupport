57595ab0eb16ac12c60cc908f9bf906f18110a3a
ritaamritkar <rita.sinkar@gmail.com>
2019-11-05 14:51:11 +0400

    added configuration as we removed the rails_12factor gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,4 +63,12 @@ LocalSupport::Application.configure do
 
   config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
   config.eager_load = true
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
